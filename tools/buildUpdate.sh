8ce40e9cf05cc2cac0a679d6430e81d382907821
Modun Zhang <modunzhang@gmail.com>
2014-08-04 17:26:08 +0800

    commit any uncommitted files

--- tools/buildUpdate.sh
+++ tools/buildUpdate.sh
@@ -18,7 +18,6 @@ cd ..
 
 echo ---------------- 同步代码
 cp -r ../update ../../server/update-server/public
-
 rsync -rave "ssh -i /Users/modun/.ssh/ModunsMBP.pem" --exclude=.DS_Store*  ../update ec2-user@ec2-54-178-151-193.ap-northeast-1.compute.amazonaws.com:~/server/update-server/public/update
 
 echo ---------------- 提交代码
@@ -26,6 +25,6 @@ cd ..
 git add --all .
 git commit -m "update new version"
 git push
-cd buildUpdate
+cd tools
 
 echo ---------------- 自动更新完成
