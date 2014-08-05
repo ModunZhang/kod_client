08a5b98720c6f0f8f177a6e2f37a1c47f393ee6a
Modun Zhang <modunzhang@gmail.com>
2014-08-05 23:11:32 +0800

    modify config

--- dev/scripts/app/datas/LocationInitData_locations.lua
+++ dev/scripts/app/datas/LocationInitData_locations.lua
@@ -2,65 +2,81 @@ local locations = GameDatas.LocationInitData.locations
 
 locations[1] = {
 	["location"] = 1,
-	["type"] = "keep"
+	["type"] = "keep",
+	["desc"] = "城堡"
 }
 locations[2] = {
 	["location"] = 2,
-	["type"] = "watchTower"
+	["type"] = "watchTower",
+	["desc"] = "瞭望塔"
 }
 locations[3] = {
 	["location"] = 3,
-	["type"] = "warehouse"
+	["type"] = "warehouse",
+	["desc"] = "资源仓库"
 }
 locations[4] = {
 	["location"] = 4,
-	["type"] = "dragonEyire"
+	["type"] = "dragonEyire",
+	["desc"] = "龙巢"
 }
 locations[5] = {
 	["location"] = 5,
-	["type"] = "hospital"
+	["type"] = "hospital",
+	["desc"] = "医院"
 }
 locations[6] = {
 	["location"] = 6,
-	["type"] = "materialDepot"
+	["type"] = "materialDepot",
+	["desc"] = "材料库房"
 }
 locations[7] = {
 	["location"] = 7,
-	["type"] = "armyCamp"
+	["type"] = "armyCamp",
+	["desc"] = "军用帐篷"
 }
 locations[8] = {
 	["location"] = 8,
-	["type"] = "barracks"
+	["type"] = "barracks",
+	["desc"] = "兵营"
 }
 locations[9] = {
 	["location"] = 9,
-	["type"] = "blackSmith"
+	["type"] = "blackSmith",
+	["desc"] = "铁匠铺"
 }
 locations[10] = {
 	["location"] = 10,
-	["type"] = "foundry"
+	["type"] = "foundry",
+	["desc"] = "锻造工坊"
 }
 locations[11] = {
 	["location"] = 11,
-	["type"] = "stoneMason"
+	["type"] = "stoneMason",
+	["desc"] = "石匠工坊"
 }
 locations[12] = {
 	["location"] = 12,
-	["type"] = "lumbermill"
+	["type"] = "lumbermill",
+	["desc"] = "锯木工房"
 }
 locations[13] = {
 	["location"] = 13,
-	["type"] = "mill"
+	["type"] = "mill",
+	["desc"] = "磨坊"
 }
 locations[14] = {
 	["location"] = 14,
-	["type"] = "toolShop"
+	["type"] = "toolShop",
+	["desc"] = "工具作坊"
 }
 locations[15] = {
 	["location"] = 15,
-	["type"] = "townHall"
+	["type"] = "townHall",
+	["desc"] = "市政厅"
 }
 locations[16] = {
 	["location"] = 16,
-	["type"] = "academy"
+	["type"] = "academy",
+	["desc"] = "学院"
 }
