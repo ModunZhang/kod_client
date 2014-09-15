eadf64a1db1ea070f7df4559cdc2e38de3484209
gaozhou <gaozhou.f@qq.com>
2014-09-15 15:52:41 +0800

    重构

--- dev/scripts/app/ui/GameUIDwelling.lua
+++ dev/scripts/app/ui/GameUIDwelling.lua
@@ -2,6 +2,7 @@
 -- Author: gaozhou
 -- Date: 2014-08-18 14:33:28
 --
+local ResourceManager = import("..entity.ResourceManager")
 local GameUIResource = import(".GameUIResource")
 local GameUIDwelling = class("GameUIDwelling", GameUIResource)
 GameUIDwelling.CITIZEN_TYPE = {
@@ -11,6 +12,51 @@ GameUIDwelling.CITIZEN_TYPE = {
     IRON = 2,
     STONE = 1
 }
+local STONE = 1
+local IRON = 2
+local WOOD = 3
+local FOOD = 4
+local CITIZEN = 5
+local items = {
+    [CITIZEN] = {
+        production_text = _("空闲城民"),
+        production_per_hour_text = _("城民增长"),
+        tag_color = "dwelling/green_head.png",
+        tag_icon = "home/res_citizen.png",
+        tag_icon_scale = 0.8
+    },
+    [FOOD] = {
+        production_text = _("农夫"),
+        production_per_hour_text = _("粮食产量"),
+        tag_color = "dwelling/yellow_head.png",
+        tag_icon = "home/res_food.png",
+        tag_icon_scale = 0.25
+    },
+    [WOOD] = {
+        production_text = _("伐木工"),
+        production_per_hour_text = _("木材产量"),
+        tag_color = "dwelling/brown_head.png",
+        tag_icon = "home/res_wood.png",
+        tag_icon_scale = 0.25
+    },
+    [IRON] = {
+        production_text = _("矿工"),
+        production_per_hour_text = _("矿产产量"),
+        tag_color = "dwelling/blue_head.png",
+        tag_icon = "home/res_iron.png",
+        tag_icon_scale = 0.25
+    },
+    [STONE] = {
+        production_text = _("石匠"),
+        production_per_hour_text = _("石料产量"),
+        tag_color = "dwelling/grey_head.png",
+        tag_icon = "home/res_stone.png",
+        tag_icon_scale = 0.25
+    }
+}
+local function return_item_info(res_type)
+    return items[res_type]
+end
 function GameUIDwelling:ctor(building, city)
     GameUIDwelling.super.ctor(self, building)
     self.dwelling_city = city
@@ -33,9 +79,11 @@ function GameUIDwelling:onMovieOutStage()
 end
 function GameUIDwelling:CreateCitizenPanel()
     local citizen_layer = display.newNode():addTo(self)
+
     local iconBg = cc.ui.UIImage.new("dwelling/citizen_bg.png")
         :pos(display.left + 45, display.top - 150)
         :addTo(citizen_layer)
+
     cc.ui.UIImage.new("home/res_citizen.png")
         :addTo(iconBg)
 
@@ -76,34 +124,42 @@ function GameUIDwelling:CreateCitizenPanel()
     }):addTo(citizen_layer)
         :align(display.LEFT_CENTER, display.left + 45, display.top - 150 - 702 - 10)
 
+
     local dwelling = self
     citizen_layer.citizen_ui = {}
-    citizen_layer.citizen_ui[dwelling.CITIZEN_TYPE.CITIZEN] = cc.ui.UIImage.new("dwelling/green_line.png"):addTo(citizen_num_bg)
-    citizen_layer.citizen_ui[dwelling.CITIZEN_TYPE.FOOD] = cc.ui.UIImage.new("dwelling/yellow_line.png"):addTo(citizen_num_bg)
-    citizen_layer.citizen_ui[dwelling.CITIZEN_TYPE.WOOD] = cc.ui.UIImage.new("dwelling/brown_line.png"):addTo(citizen_num_bg)
-    citizen_layer.citizen_ui[dwelling.CITIZEN_TYPE.IRON] = cc.ui.UIImage.new("dwelling/blue_line.png"):addTo(citizen_num_bg)
-    citizen_layer.citizen_ui[dwelling.CITIZEN_TYPE.STONE] = cc.ui.UIImage.new("dwelling/grey_line.png"):addTo(citizen_num_bg)
+    citizen_layer.citizen_ui[CITIZEN] = cc.ui.UIImage.new("dwelling/green_line.png"):addTo(citizen_num_bg)
+    citizen_layer.citizen_ui[FOOD] = cc.ui.UIImage.new("dwelling/yellow_line.png"):addTo(citizen_num_bg)
+    citizen_layer.citizen_ui[WOOD] = cc.ui.UIImage.new("dwelling/brown_line.png"):addTo(citizen_num_bg)
+    citizen_layer.citizen_ui[IRON] = cc.ui.UIImage.new("dwelling/blue_line.png"):addTo(citizen_num_bg)
+    citizen_layer.citizen_ui[STONE] = cc.ui.UIImage.new("dwelling/grey_line.png"):addTo(citizen_num_bg)
 
 
     citizen_layer.citizen_number = {}
     local end_pos = 720
     local count = #citizen_layer.citizen_ui
     for i, v in pairs(citizen_layer.citizen_ui) do
+
+        local item_info = return_item_info(i)
+
         local cur_pos = end_pos - (count - i) * 100
+
         local res_info_bg = cc.ui.UIImage.new("dwelling/res_info_bg.png"):addTo(citizen_layer):pos(205, cur_pos)
+
         cc.ui.UIImage.new("dwelling/dividing_line.png"):addTo(res_info_bg):pos(0, 43)
+
         cc.ui.UILabel.new({
             UILabelType = cc.ui.UILabel.LABEL_TYPE_TTF,
-            text = _("空闲城民"),
+            text = item_info.production_text,
             font = UIKit:getFontFilePath(),
             size = 30,
             align = cc.ui.UILabel.TEXT_ALIGN_LEFT,
             color = UIKit:hex2c3b(0x797154),
             valign = cc.ui.UILabel.TEXT_VALIGN_CENTER
         }):addTo(res_info_bg):align(display.LEFT_CENTER, 45, 65)
+
         cc.ui.UILabel.new({
             UILabelType = cc.ui.UILabel.LABEL_TYPE_TTF,
-            text = _("城民增长"),
+            text = item_info.production_per_hour_text,
             font = UIKit:getFontFilePath(),
             size = 30,
             align = cc.ui.UILabel.TEXT_ALIGN_LEFT,
@@ -131,40 +187,18 @@ function GameUIDwelling:CreateCitizenPanel()
             valign = cc.ui.UILabel.TEXT_VALIGN_CENTER
         }):addTo(res_info_bg):align(display.RIGHT_CENTER, 350, 20)
 
-        if i == dwelling.CITIZEN_TYPE.CITIZEN then
-            local head = cc.ui.UIImage.new("dwelling/green_head.png"):addTo(res_info_bg):pos(0, 2)
-            local res_bg = cc.ui.UIImage.new("dwelling/res_bg.png"):addTo(head):align(display.CENTER, 20, 40)
-            local res_bg_pos = res_bg:getAnchorPointInPoints()
-            cc.ui.UIImage.new("home/res_citizen.png"):addTo(res_bg):scale(0.8):align(display.CENTER, res_bg_pos.x, res_bg_pos.y)
+        local head = cc.ui.UIImage.new(item_info.tag_color):addTo(res_info_bg):pos(0, 2)
+        local res_bg = cc.ui.UIImage.new("dwelling/res_bg.png"):addTo(head):align(display.CENTER, 20, 40)
+        local res_bg_pos = res_bg:getAnchorPointInPoints()
+        cc.ui.UIImage.new(item_info.tag_icon):addTo(res_bg):scale(item_info.tag_icon_scale):align(display.CENTER, res_bg_pos.x, res_bg_pos.y)
 
+        if i == CITIZEN then
             local add_btn = cc.ui.UIPushButton.new(
-            {normal = "dwelling/add_btn_up.png",pressed = "dwelling/add_btn_down.png"})
-            :addTo(res_info_bg):pos(375, 43)
+                {normal = "dwelling/add_btn_up.png",pressed = "dwelling/add_btn_down.png"})
+                :addTo(res_info_bg):pos(375, 43)
             cc.ui.UIImage.new("dwelling/add.png"):addTo(add_btn):align(display.CENTER, 0, 0)
-
-
-
-        elseif i == dwelling.CITIZEN_TYPE.FOOD then
-            local head = cc.ui.UIImage.new("dwelling/yellow_head.png"):addTo(res_info_bg):pos(0, 2)
-            local res_bg = cc.ui.UIImage.new("dwelling/res_bg.png"):addTo(head):align(display.CENTER, 20, 40)
-            local res_bg_pos = res_bg:getAnchorPointInPoints()
-            cc.ui.UIImage.new("home/res_food.png"):addTo(res_bg):scale(0.25):align(display.CENTER, res_bg_pos.x, res_bg_pos.y)
-        elseif i == dwelling.CITIZEN_TYPE.WOOD then
-            local head = cc.ui.UIImage.new("dwelling/brown_head.png"):addTo(res_info_bg):pos(0, 2)
-            local res_bg = cc.ui.UIImage.new("dwelling/res_bg.png"):addTo(head):align(display.CENTER, 20, 40)
-            local res_bg_pos = res_bg:getAnchorPointInPoints()
-            cc.ui.UIImage.new("home/res_wood.png"):addTo(res_bg):scale(0.25):pos(5, 5):align(display.CENTER, res_bg_pos.x, res_bg_pos.y)
-        elseif i == dwelling.CITIZEN_TYPE.IRON then
-            local head = cc.ui.UIImage.new("dwelling/blue_head.png"):addTo(res_info_bg):pos(0, 2)
-            local res_bg = cc.ui.UIImage.new("dwelling/res_bg.png"):addTo(head):align(display.CENTER, 20, 40)
-            local res_bg_pos = res_bg:getAnchorPointInPoints()
-            cc.ui.UIImage.new("home/res_iron.png"):addTo(res_bg):scale(0.25):pos(5, 5):align(display.CENTER, res_bg_pos.x, res_bg_pos.y)
-        elseif i == dwelling.CITIZEN_TYPE.STONE then
-            local head = cc.ui.UIImage.new("dwelling/grey_head.png"):addTo(res_info_bg):pos(0, 2)
-            local res_bg = cc.ui.UIImage.new("dwelling/res_bg.png"):addTo(head):align(display.CENTER, 20, 40)
-            local res_bg_pos = res_bg:getAnchorPointInPoints()
-            cc.ui.UIImage.new("home/res_coin.png"):addTo(res_bg):scale(0.25):pos(5, 5):align(display.CENTER, res_bg_pos.x, res_bg_pos.y)
         end
+
         citizen_layer.citizen_number[i] = {production, productionPerHour}
     end
 
@@ -173,11 +207,11 @@ function GameUIDwelling:CreateCitizenPanel()
         local city = dwelling.dwelling_city
         citizen_array = {}
         local resource_manager = city:GetResourceManager()
-        citizen_array[dwelling.CITIZEN_TYPE.CITIZEN] = resource_manager:GetPopulationResource():GetNoneAllocatedByTime(app.timer:GetServerTime())
-        citizen_array[dwelling.CITIZEN_TYPE.FOOD] = city:GetCitizenByType("farmer")
-        citizen_array[dwelling.CITIZEN_TYPE.WOOD] = city:GetCitizenByType("woodcutter")
-        citizen_array[dwelling.CITIZEN_TYPE.IRON] = city:GetCitizenByType("miner")
-        citizen_array[dwelling.CITIZEN_TYPE.STONE] = city:GetCitizenByType("quarrier")
+        citizen_array[CITIZEN] = resource_manager:GetPopulationResource():GetNoneAllocatedByTime(app.timer:GetServerTime())
+        citizen_array[FOOD] = city:GetCitizenByType("farmer")
+        citizen_array[WOOD] = city:GetCitizenByType("woodcutter")
+        citizen_array[IRON] = city:GetCitizenByType("miner")
+        citizen_array[STONE] = city:GetCitizenByType("quarrier")
         self:SetMaxCitizen(resource_manager:GetPopulationResource():GetTotalLimit())
         self:OnCitizenChanged(citizen_array)
     end
@@ -201,16 +235,16 @@ function GameUIDwelling:CreateCitizenPanel()
         local resource_manager = dwelling.dwelling_city:GetResourceManager()
         for k, v in pairs(citizen_layer.citizen_number) do
             local production = string.format("%d", citizen_array[k])
-            local productionPerHour 
-            if k == GameUIDwelling.CITIZEN_TYPE.CITIZEN then
+            local productionPerHour
+            if k == CITIZEN then
                 productionPerHour = resource_manager:GetPopulationResource():GetProductionPerHour()
-            elseif k == GameUIDwelling.CITIZEN_TYPE.FOOD then
+            elseif k == FOOD then
                 productionPerHour = resource_manager:GetFoodResource():GetProductionPerHour()
-            elseif k == GameUIDwelling.CITIZEN_TYPE.WOOD then
+            elseif k == WOOD then
                 productionPerHour = resource_manager:GetWoodResource():GetProductionPerHour()
-            elseif k == GameUIDwelling.CITIZEN_TYPE.IRON then
+            elseif k == IRON then
                 productionPerHour = resource_manager:GetIronResource():GetProductionPerHour()
-            elseif k == GameUIDwelling.CITIZEN_TYPE.STONE then
+            elseif k == STONE then
                 productionPerHour = resource_manager:GetStoneResource():GetProductionPerHour()
             end
             v[1]:setString(production)
@@ -293,6 +327,9 @@ function GameUIDwelling:OnResourceChanged(resource_manager)
     end
 end
 return GameUIDwelling
+
+
+
 
 
 
