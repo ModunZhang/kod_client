0a813d6f768b0b68ae0cd00e49148f87cb7fd425
gaozhou <gaozhou.f@qq.com>
2014-09-16 15:07:08 +0800

    重构

---
+++ dev/scripts/app/entity/BuildingRegister.lua
@@ -0,0 +1,14 @@
+local UpgradeBuilding = import("..entity.UpgradeBuilding")
+local BuildingRegister = {
+	keep 			= import("..entity.KeepUpgradeBuilding"),
+    warehouse 		= import("..entity.WarehouseUpgradeBuilding"),
+    woodcutter 		= import("..entity.WoodResourceUpgradeBuilding"),
+    farmer 			= import("..entity.FoodResourceUpgradeBuilding"),
+    miner 			= import("..entity.IronResourceUpgradeBuilding"),
+    quarrier 		= import("..entity.StoneResourceUpgradeBuilding"),
+    dwelling 		= import("..entity.PopulationResourceUpgradeBuilding"),
+}
+setmetatable(BuildingRegister, {__index = function(t, k, v)
+	return UpgradeBuilding
+end})
+return BuildingRegister
