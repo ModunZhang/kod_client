f351265e22bfa35b9db8768f5e6496fd8d5e0973
gaozhou <gaozhou.f@qq.com>
2014-10-11 11:24:25 +0800

    添加联盟地图

--- dev/scripts/app/layers/CityLayer.lua
+++ dev/scripts/app/layers/CityLayer.lua
@@ -28,8 +28,8 @@ function CityLayer:GetClickedObject(x, y, world_x, world_y)
     }
     self:IteratorClickAble(function(k, v)
         if not v:isVisible() then return false end
-        -- if v:GetEntity():GetType() == "wall" and not v:GetEntity():IsGate() then return false end
-        -- if v:GetEntity():GetType() == "tower" and not v:GetEntity():IsUnlocked() then return false end
+        if v:GetEntity():GetType() == "wall" and not v:GetEntity():IsGate() then return false end
+        if v:GetEntity():GetType() == "tower" and not v:GetEntity():IsUnlocked() then return false end
 
         local check = v:IsContainPointWithFullCheck(x, y, world_x, world_y)
         if check.logic_clicked then
