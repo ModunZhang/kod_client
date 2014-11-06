f2a573380d750c9b7587a3fb180b38e12240abe9
gaozhou <gaozhou.f@qq.com>
2014-11-06 19:19:11 +0800

    rebuild

---
+++ dev/scripts/app/datas/GameDatas.lua
@@ -0,0 +1,293 @@
+GameDatas = {}
+
+GameDatas.ClientInitGame = {
+	["ruins"] = {},
+	["locations"] = {},
+}
+require("app.datas.ClientInitGame_ruins")
+require("app.datas.ClientInitGame_locations")
+
+GameDatas.AllianceBuilding = {
+	["palace"] = {},
+	["moonGate"] = {},
+	["orderHall"] = {},
+	["shrine"] = {},
+	["allianceShop"] = {},
+}
+require("app.datas.AllianceBuilding_palace")
+require("app.datas.AllianceBuilding_moonGate")
+require("app.datas.AllianceBuilding_orderHall")
+require("app.datas.AllianceBuilding_shrine")
+require("app.datas.AllianceBuilding_allianceShop")
+
+GameDatas.AllianceInitData = {
+	["intInit"] = {},
+	["stringInit"] = {},
+	["right"] = {},
+	["buildingType"] = {},
+	["donate"] = {},
+	["decorateCount"] = {},
+}
+require("app.datas.AllianceInitData_intInit")
+require("app.datas.AllianceInitData_stringInit")
+require("app.datas.AllianceInitData_right")
+require("app.datas.AllianceInitData_buildingType")
+require("app.datas.AllianceInitData_donate")
+require("app.datas.AllianceInitData_decorateCount")
+
+GameDatas.AllianceShrine = {
+	["shrineStage"] = {},
+}
+require("app.datas.AllianceShrine_shrineStage")
+
+GameDatas.AllianceVillage = {
+	["woodVillage"] = {},
+	["foodVillage"] = {},
+	["stoneVillage"] = {},
+	["ironVillage"] = {},
+	["coinVillage"] = {},
+	["gemVillage"] = {},
+}
+require("app.datas.AllianceVillage_woodVillage")
+require("app.datas.AllianceVillage_foodVillage")
+require("app.datas.AllianceVillage_stoneVillage")
+require("app.datas.AllianceVillage_ironVillage")
+require("app.datas.AllianceVillage_coinVillage")
+require("app.datas.AllianceVillage_gemVillage")
+
+GameDatas.BuildingFunction = {
+	["wall"] = {},
+	["tower"] = {},
+	["keep"] = {},
+	["watchTower"] = {},
+	["dragonEyrie"] = {},
+	["warehouse"] = {},
+	["barracks"] = {},
+	["armyCamp"] = {},
+	["blackSmith"] = {},
+	["materialDepot"] = {},
+	["toolShop"] = {},
+	["lumbermill"] = {},
+	["stoneMason"] = {},
+	["foundry"] = {},
+	["mill"] = {},
+	["townHall"] = {},
+	["acdemy"] = {},
+	["hospital"] = {},
+	["tradeGuild"] = {},
+	["prison"] = {},
+	["trainingGround"] = {},
+	["hunterhall"] = {},
+	["stable"] = {},
+	["workshop"] = {},
+}
+require("app.datas.BuildingFunction_wall")
+require("app.datas.BuildingFunction_tower")
+require("app.datas.BuildingFunction_keep")
+require("app.datas.BuildingFunction_watchTower")
+require("app.datas.BuildingFunction_dragonEyrie")
+require("app.datas.BuildingFunction_warehouse")
+require("app.datas.BuildingFunction_barracks")
+require("app.datas.BuildingFunction_armyCamp")
+require("app.datas.BuildingFunction_blackSmith")
+require("app.datas.BuildingFunction_materialDepot")
+require("app.datas.BuildingFunction_toolShop")
+require("app.datas.BuildingFunction_lumbermill")
+require("app.datas.BuildingFunction_stoneMason")
+require("app.datas.BuildingFunction_foundry")
+require("app.datas.BuildingFunction_mill")
+require("app.datas.BuildingFunction_townHall")
+require("app.datas.BuildingFunction_acdemy")
+require("app.datas.BuildingFunction_hospital")
+require("app.datas.BuildingFunction_tradeGuild")
+require("app.datas.BuildingFunction_prison")
+require("app.datas.BuildingFunction_trainingGround")
+require("app.datas.BuildingFunction_hunterhall")
+require("app.datas.BuildingFunction_stable")
+require("app.datas.BuildingFunction_workshop")
+
+GameDatas.BuildingLevelUp = {
+	["wall"] = {},
+	["tower"] = {},
+	["keep"] = {},
+	["watchTower"] = {},
+	["dragonEyrie"] = {},
+	["warehouse"] = {},
+	["barracks"] = {},
+	["armyCamp"] = {},
+	["blackSmith"] = {},
+	["materialDepot"] = {},
+	["toolShop"] = {},
+	["lumbermill"] = {},
+	["stoneMason"] = {},
+	["foundry"] = {},
+	["mill"] = {},
+	["townHall"] = {},
+	["acdemy"] = {},
+	["hospital"] = {},
+	["tradeGuild"] = {},
+	["prison"] = {},
+	["trainingGround"] = {},
+	["hunterhall"] = {},
+	["stable"] = {},
+	["workshop"] = {},
+}
+require("app.datas.BuildingLevelUp_wall")
+require("app.datas.BuildingLevelUp_tower")
+require("app.datas.BuildingLevelUp_keep")
+require("app.datas.BuildingLevelUp_watchTower")
+require("app.datas.BuildingLevelUp_dragonEyrie")
+require("app.datas.BuildingLevelUp_warehouse")
+require("app.datas.BuildingLevelUp_barracks")
+require("app.datas.BuildingLevelUp_armyCamp")
+require("app.datas.BuildingLevelUp_blackSmith")
+require("app.datas.BuildingLevelUp_materialDepot")
+require("app.datas.BuildingLevelUp_toolShop")
+require("app.datas.BuildingLevelUp_lumbermill")
+require("app.datas.BuildingLevelUp_stoneMason")
+require("app.datas.BuildingLevelUp_foundry")
+require("app.datas.BuildingLevelUp_mill")
+require("app.datas.BuildingLevelUp_townHall")
+require("app.datas.BuildingLevelUp_acdemy")
+require("app.datas.BuildingLevelUp_hospital")
+require("app.datas.BuildingLevelUp_tradeGuild")
+require("app.datas.BuildingLevelUp_prison")
+require("app.datas.BuildingLevelUp_trainingGround")
+require("app.datas.BuildingLevelUp_hunterhall")
+require("app.datas.BuildingLevelUp_stable")
+require("app.datas.BuildingLevelUp_workshop")
+
+GameDatas.Buildings = {
+	["buildings"] = {},
+}
+require("app.datas.Buildings_buildings")
+
+GameDatas.DragonEyrie = {
+	["crown"] = {},
+	["armguardLeft"] = {},
+	["armguardRight"] = {},
+	["chest"] = {},
+	["sting"] = {},
+	["orb"] = {},
+	["equipmentBuff"] = {},
+	["dragonAttribute"] = {},
+	["dragons"] = {},
+	["dragonSkill"] = {},
+}
+require("app.datas.DragonEyrie_crown")
+require("app.datas.DragonEyrie_armguardLeft")
+require("app.datas.DragonEyrie_armguardRight")
+require("app.datas.DragonEyrie_chest")
+require("app.datas.DragonEyrie_sting")
+require("app.datas.DragonEyrie_orb")
+require("app.datas.DragonEyrie_equipmentBuff")
+require("app.datas.DragonEyrie_dragonAttribute")
+require("app.datas.DragonEyrie_dragons")
+require("app.datas.DragonEyrie_dragonSkill")
+
+GameDatas.GemsPayment = {
+	["time"] = {},
+	["wood"] = {},
+	["stone"] = {},
+	["iron"] = {},
+	["food"] = {},
+	["coin"] = {},
+	["citizen"] = {},
+	["material"] = {},
+}
+require("app.datas.GemsPayment_time")
+require("app.datas.GemsPayment_wood")
+require("app.datas.GemsPayment_stone")
+require("app.datas.GemsPayment_iron")
+require("app.datas.GemsPayment_food")
+require("app.datas.GemsPayment_coin")
+require("app.datas.GemsPayment_citizen")
+require("app.datas.GemsPayment_material")
+
+GameDatas.HouseFunction = {
+	["dwelling"] = {},
+	["woodcutter"] = {},
+	["quarrier"] = {},
+	["miner"] = {},
+	["farmer"] = {},
+}
+require("app.datas.HouseFunction_dwelling")
+require("app.datas.HouseFunction_woodcutter")
+require("app.datas.HouseFunction_quarrier")
+require("app.datas.HouseFunction_miner")
+require("app.datas.HouseFunction_farmer")
+
+GameDatas.HouseLevelUp = {
+	["dwelling"] = {},
+	["woodcutter"] = {},
+	["farmer"] = {},
+	["quarrier"] = {},
+	["miner"] = {},
+}
+require("app.datas.HouseLevelUp_dwelling")
+require("app.datas.HouseLevelUp_woodcutter")
+require("app.datas.HouseLevelUp_farmer")
+require("app.datas.HouseLevelUp_quarrier")
+require("app.datas.HouseLevelUp_miner")
+
+GameDatas.HouseReturn = {
+	["dwelling"] = {},
+	["woodcutter"] = {},
+	["quarrier"] = {},
+	["miner"] = {},
+	["farmer"] = {},
+}
+require("app.datas.HouseReturn_dwelling")
+require("app.datas.HouseReturn_woodcutter")
+require("app.datas.HouseReturn_quarrier")
+require("app.datas.HouseReturn_miner")
+require("app.datas.HouseReturn_farmer")
+
+GameDatas.Houses = {
+	["houses"] = {},
+}
+require("app.datas.Houses_houses")
+
+GameDatas.PlayerInitData = {
+	["resources"] = {},
+	["materials"] = {},
+	["soldierMaterials"] = {},
+	["dragonMaterials"] = {},
+	["houses"] = {},
+	["playerLevel"] = {},
+	["vipLevel"] = {},
+}
+require("app.datas.PlayerInitData_resources")
+require("app.datas.PlayerInitData_materials")
+require("app.datas.PlayerInitData_soldierMaterials")
+require("app.datas.PlayerInitData_dragonMaterials")
+require("app.datas.PlayerInitData_houses")
+require("app.datas.PlayerInitData_playerLevel")
+require("app.datas.PlayerInitData_vipLevel")
+
+GameDatas.PlayerVillageExp = {
+	["exp"] = {},
+	["wood"] = {},
+	["stone"] = {},
+	["iron"] = {},
+	["food"] = {},
+	["coin"] = {},
+}
+require("app.datas.PlayerVillageExp_exp")
+require("app.datas.PlayerVillageExp_wood")
+require("app.datas.PlayerVillageExp_stone")
+require("app.datas.PlayerVillageExp_iron")
+require("app.datas.PlayerVillageExp_food")
+require("app.datas.PlayerVillageExp_coin")
+
+GameDatas.SmithConfig = {
+	["equipments"] = {},
+}
+require("app.datas.SmithConfig_equipments")
+
+GameDatas.UnitsConfig = {
+	["normal"] = {},
+	["special"] = {},
+}
+require("app.datas.UnitsConfig_normal")
+require("app.datas.UnitsConfig_special")
