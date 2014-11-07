950b6458890a773e682c8ff04493be5d74cd8352
gaozhou <gaozhou.f@qq.com>
2014-11-07 10:44:08 +0800

    重载动画

--- dev/scripts/app/ui/Wall.lua
+++ dev/scripts/app/ui/Wall.lua
@@ -9,33 +9,16 @@ function Wall:ctor()
     self.wall:getAnimation():setMovementEventCallFunc(handler(self, self.OnAnimationCallback))
 end
 function Wall:PlayAnimation(ani, loop_time)
-    if ani ~= "hurt" and ani ~= "attack" then
-        return
-    end
     self.wall:getAnimation():play(ani, -1, loop_time or -1)
-end
-function Wall:OnAnimationStart(animation_name)
-
-end
-function Wall:OnAnimationComplete(animation_name)
-
-end
-function Wall:OnAnimationPlayEnd(ani_name, func)
-    if ani_name ~= "hurt" and ani_name ~= "attack" then
-        func()
-        return
-    end
-    assert(type(func) == "function")
-    if not self.callback_map[ani_name] then
-        self.callback_map[ani_name] = {}
-    end
-    table.insert(self.callback_map[ani_name], func)
 end
 function Wall:turnLeft()
     self:setScaleX(1)
 end
 function Wall:turnRight()
     self:setScaleX(-1)
+end
+function Wall:breath()
+    return promise.new()
 end
 return Wall
 
