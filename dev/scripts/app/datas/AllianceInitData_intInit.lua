c9327aa61ab3f82f025743316a5f11302755dc63
DannyHe <dannyjiajia@gmail.com>
2014-11-07 15:42:44 +0800

    更新配置文件

---
+++ dev/scripts/app/datas/AllianceInitData_intInit.lua
@@ -0,0 +1,22 @@
+local intInit = GameDatas.AllianceInitData.intInit
+
+intInit["createAlliance"] = {
+	["type"] = "createAlliance",
+	["value"] = 50
+}
+intInit["buyArchon"] = {
+	["type"] = "buyArchon",
+	["value"] = 100
+}
+intInit["editAllianceBasicInfo"] = {
+	["type"] = "editAllianceBasicInfo",
+	["value"] = 50
+}
+intInit["editAllianceTerrian"] = {
+	["type"] = "editAllianceTerrian",
+	["value"] = 50
+}
+intInit["activeShrineStageEvent"] = {
+	["type"] = "activeShrineStageEvent",
+	["value"] = 3600
+}
