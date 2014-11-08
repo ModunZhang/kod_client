eae7b5af5f7a480503fa549a9eacb8acc31961e8
DannyHe <dannyjiajia@gmail.com>
2014-11-08 08:56:50 +0800

    联盟圣地

--- dev/scripts/app/entity/AllianceShrine.lua
+++ dev/scripts/app/entity/AllianceShrine.lua
@@ -3,10 +3,21 @@
 -- Date: 2014-11-07 15:21:22
 --
 local config_shrineStage = GameDatas.AllianceShrine.shrineStage
+local AllianceShrineStage = import(".AllianceShrineStage")
 local AllianceShrine = class("AllianceShrine")
+local property = import("..utils.property")
 
 function AllianceShrine:ctor()
-
+	local states_ = {}
+	table.foreach(config_shrineStage,function(key,config)
+		if string.sub(key,1,1) == tostring(state) then 
+			local state = AllianceShrineStage.new()
+			state:loadProperty(config)
+			states_[key] = state
+		end 
+	end)
+	property(self,"states",states_)
+	dump(self)
 end
 
 
@@ -15,15 +26,15 @@ function AllianceShrine:GetStatgeByName(state_name)
 	return config_shrineStage[state_name]
 end
 
-
-function AllianceShrine:GetState(state)
-	local states = {}
-	table.foreach(config_shrineStage,function(key,state)
-		if string.sub(key,1,1) == tostring(state) then 
-			table.insert(states,state)
-		end 
-	end)
-	return states
+-- state is number
+function AllianceShrine:GetStateByMainState(state_index)
+	local tempStages = {}
+	for key,state in pairs(self:States()) do
+		if tonumber(string.sub(key,1,1)) == state_index then
+			table.insert(tempStages,state)
+		end
+	end
+	return tempStages
 end
 
 return AllianceShrine
---
+++ dev/scripts/app/entity/AllianceShrineStage.lua
@@ -0,0 +1,32 @@
+--
+-- Author: Danny He
+-- Date: 2014-11-07 17:09:04
+--
+-- 联盟圣地关卡
+local AllianceShrineStage = class("AllianceShrineStage")
+local property = import("..utils.property")
+
+
+function AllianceShrineStage:ctor()
+end
+
+function AllianceShrineStage:loadProperty(config)
+	property(self,"stageName",config.stageName)
+	property(self,"stage",config.stage)
+	property(self,"subStage",config.subStage)
+	property(self,"enemyPower",config.enemyPower)
+	property(self,"needPerception",config.needPerception)
+	property(self,"suggestPlayer",config.suggestPlayer)
+	property(self,"suggestPower",config.suggestPower)
+	--TODO:troops
+	property(self,"star2DeathPopulation",config.star2DeathPopulation)
+	property(self,"star1Honour",config.star1Honour)
+	property(self,"star2Honour",config.star2Honour)
+	property(self,"star3Honour",config.star3Honour)
+	property(self,"bronzeKill",config.bronzeKill)
+	property(self,"silverKill",config.silverKill)
+	property(self,"goldKill",config.goldKill)
+	--TODO:rewards
+end
+
+return AllianceShrineStage
