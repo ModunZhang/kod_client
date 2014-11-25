5dde62521627fa0e7ad3b3a8ee64b6c8ce2c93ff
DannyHe <dannyjiajia@gmail.com>
2014-11-25 16:46:40 +0800

    协防行军事件

---
+++ dev/scripts/app/entity/HelpDefenceMarchEvent.lua
@@ -0,0 +1,46 @@
+--
+-- Author: Danny He
+-- Date: 2014-11-25 16:05:13
+--
+local Observer = import(".Observer")
+local HelpDefenceMarchEvent = class("HelpDefenceMarchEvent",Observer)
+local property = import("..utils.property")
+
+function HelpDefenceMarchEvent:ctor()
+	property(self,"id","")
+	property(self,"playerData","")
+	property(self,"targetPlayerData","")
+	property(self,"startTime","")
+	property(self,"arriveTime","")
+	property(self,"targetLocation","") -- set from outsider
+	property(self,"fromLocation","") -- set from outsider
+
+end
+
+function HelpDefenceMarchEvent:Update(json_data)
+	self:SetId(json_data.id)
+	self:SetPlayerData(json_data.playerData)
+	self:SetTargetPlayerData(json_data.targetPlayerData)
+	self:SetStartTime(json_data.startTime)
+	self:SetArriveTime(json_data.arriveTime)
+end
+
+function HelpDefenceMarchEvent:OnTimer(current_time)
+	self.times = math.ceil(self:ArriveTime() - current_time)
+	if self.times >= 0 then
+		self:NotifyObservers(function(listener)
+			listener:OnHelpDefenceMarchEventTimer(self)
+		end)
+	end
+end
+
+function HelpDefenceMarchEvent:Reset()
+	self:RemoveAllObserver()
+end
+
+function HelpDefenceMarchEvent:GetTime()
+	return self.times or 0
+end
+
+
+return HelpDefenceMarchEvent
