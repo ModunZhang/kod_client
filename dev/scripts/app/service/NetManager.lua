378ddd79d2e820473d35b0add8e459051db0c944
Kenny Dai <daidai917@gmail.com>
2014-11-26 16:57:17 +0800

    11

--- dev/scripts/app/service/NetManager.lua
+++ dev/scripts/app/service/NetManager.lua
@@ -463,8 +463,7 @@ function NetManager:getLoginPromise()
         if gaozhou then
             device_id = "b"
         else
-            -- device_id = device.getOpenUDID()
-            device_id = "aj1"
+            device_id = device.getOpenUDID()
         end
     else
         device_id = device.getOpenUDID()
