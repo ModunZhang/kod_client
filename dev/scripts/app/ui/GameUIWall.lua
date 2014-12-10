e61a477f604d7a476a0fa98eaf0d7df25ea52be5
DannyHe <dannyjiajia@gmail.com>
2014-12-10 15:16:15 +0800

    wall

--- dev/scripts/app/ui/GameUIWall.lua
+++ dev/scripts/app/ui/GameUIWall.lua
@@ -7,4 +7,20 @@ function GameUIWall:ctor(city,building)
     GameUIWall.super.ctor(self,city,bn[building:GetType()],building)
 end
 
+function GameUIWall:onEnter()
+	GameUIWall.super.onEnter(self)
+	self:CreateTabButtons({
+        {
+            label = _("驻防"),
+            tag = "hold",
+        }
+    },
+    function(tag)
+        if tag == 'hold' then
+        else
+        end
+    end)
+end
+
+
 return GameUIWall
