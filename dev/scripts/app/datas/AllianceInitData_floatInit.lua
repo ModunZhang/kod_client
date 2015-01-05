86d5dfb40f24ae5fce290537c088ac94e3911adf
DannyHe <dannyjiajia@gmail.com>
2015-01-05 15:44:42 +0800

    更新配置文件

---
+++ dev/scripts/app/datas/AllianceInitData_floatInit.lua
@@ -0,0 +1,14 @@
+local floatInit = GameDatas.AllianceInitData.floatInit
+
+floatInit["dragonExpByKilledCitizen"] = {
+	["type"] = "dragonExpByKilledCitizen",
+	["value"] = 0.500000
+}
+floatInit["bloodByKilledCitizen"] = {
+	["type"] = "bloodByKilledCitizen",
+	["value"] = 0.500000
+}
+floatInit["citizenCountPerWallHp"] = {
+	["type"] = "citizenCountPerWallHp",
+	["value"] = 1.000000
+}
