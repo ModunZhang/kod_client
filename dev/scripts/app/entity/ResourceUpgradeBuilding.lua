75b3bd19a3dbe8d88730eeca17be01b6a41f7cdf
Kenny Dai <daidai917@gmail.com>
2015-01-08 15:38:30 +0800

    升级小屋时判定是否会使空闲城民小于0

--- dev/scripts/app/entity/ResourceUpgradeBuilding.lua
+++ dev/scripts/app/entity/ResourceUpgradeBuilding.lua
@@ -26,6 +26,10 @@ function ResourceUpgradeBuilding:GetCitizen()
 	local current_config = self:IsUpgrading() and config[self:GetNextLevel()] or config[self:GetLevel()]
 	return current_config.citizen
 end
+function ResourceUpgradeBuilding:GetNextLevelCitizen()
+    local config = config_house_levelup[self:GetType()]
+    return config[self:GetNextLevel()].citizen
+end
 function ResourceUpgradeBuilding:GetProductionPerHour()
 	local config = config_house_function[self:GetType()]
 	local current_config = config[self:GetLevel()]
@@ -39,8 +43,6 @@ end
 function ResourceUpgradeBuilding:GetUpdateResourceType()
 	return nil
 end
---
-
 function ResourceUpgradeBuilding:getUpgradeNowNeedGems()
     
     local resource_config = DataUtils:getHouseUpgradeRequired(self.building_type, self.level+1)
@@ -70,96 +72,14 @@ function ResourceUpgradeBuilding:getUpgradeRequiredGems()
     required_gems = required_gems + DataUtils:buyMaterial(resource_config.materials, has_materials)
     return required_gems
 end
--- function ResourceUpgradeBuilding:GetLevelUpWood()
---     local level = self.level
---     return config_house_levelup[self:GetType()][level+1].wood
--- end
 
--- function ResourceUpgradeBuilding:GetLevelUpStone()
---     local level = self.level
---     return config_house_levelup[self:GetType()][level+1].stone
--- end
-
--- function ResourceUpgradeBuilding:GetLevelUpIron()
---     local level = self.level
---     return config_house_levelup[self:GetType()][level+1].iron
--- end
-
--- function ResourceUpgradeBuilding:GetLevelUpBlueprints()
---     local level = self.level
---     return config_house_levelup[self:GetType()][level+1].blueprints
--- end
-
--- function ResourceUpgradeBuilding:GetLevelUpTools()
---     local level = self.level
---     return config_house_levelup[self:GetType()][level+1].tools
--- end
-
--- function ResourceUpgradeBuilding:GetLevelUpTiles()
---     local level = self.level
---     return config_house_levelup[self:GetType()][level+1].tiles
--- end
-
--- function ResourceUpgradeBuilding:GetLevelUpPulley()
---     local level = self.level
---     return config_house_levelup[self:GetType()][level+1].pulley
--- end
-
--- function ResourceUpgradeBuilding:GetLevelUpBuildTime()
---     local level = self.level
---     return config_house_levelup[self:GetType()][level+1].buildTime
--- end
-
--- function ResourceUpgradeBuilding:GetLevelUpCitizen()
---     local level = self.level
---     return config_house_levelup[self:GetType()][level+1].citizen
--- end
-
--- function ResourceUpgradeBuilding:IsAbleToUpgrade()
---     local config = config_house_levelup[self:GetType()]
---     local level = self.level
---     -- 地块是否解锁
---     local tile = City:GetTileWhichBuildingBelongs(self)
---     if not City:IsUnLockedAtIndex(tile.x,tile.y) then
---         return UpgradeBuilding.NOT_ABLE_TO_UPGRADE.TILE_NOT_UNLOCKED
---     end
-
---     if City:GetBuildingByLocationId(1):GetLevel()==level then
---         return UpgradeBuilding.NOT_ABLE_TO_UPGRADE.LEVEL_CAN_NOT_HIGHER_THAN_KEEP_LEVEL
---     elseif #config == level then
---         return UpgradeBuilding.NOT_ABLE_TO_UPGRADE.IS_MAX_LEVEL
---     end
-
---     local r = {
---         WOOD = City.resource_manager.RESOURCE_TYPE.WOOD,
---         IRON = City.resource_manager.RESOURCE_TYPE.IRON,
---         STONE = City.resource_manager.RESOURCE_TYPE.STONE,
---         POPULATION = City.resource_manager.RESOURCE_TYPE.POPULATION
---     }
-
---     local wood = City.resource_manager:GetResourceByType(r.WOOD):GetResourceValueByCurrentTime(app.timer:GetServerTime())
---     local iron = City.resource_manager:GetResourceByType(r.IRON):GetResourceValueByCurrentTime(app.timer:GetServerTime())
---     local stone = City.resource_manager:GetResourceByType(r.STONE):GetResourceValueByCurrentTime(app.timer:GetServerTime())
---     local population = City.resource_manager:GetResourceByType(r.POPULATION):GetResourceValueByCurrentTime(app.timer:GetServerTime())
-    
---     -- 还未管理道具，暂时从userdata中取
---     local m = DataManager:getUserData().materials
---     -- print("wood ",wood,config[level+1].wood)
---     -- print("population ",population,config[level+1].citizen)
---     -- print("iron ",iron,config[level+1].iron)
---     -- print("stone ",stone,config[level+1].stone)
---     -- print("tiles ",m.tiles,config[level+1].tiles)
---     -- print("tools ",m.tools,config[level+1].tools)
---     -- print("blueprints ",m.blueprints,config[level+1].blueprints)
---     -- print("pulley ",m.pulley,config[level+1].pulley)
-
---     if wood<config[level+1].wood or population<config[level+1].citizen
---         or stone<config[level+1].stone or iron<config[level+1].iron
---         or m.tiles<config[level+1].tiles or m.tools<config[level+1].tools
---         or m.blueprints<config[level+1].blueprints or m.pulley<config[level+1].pulley
---     then
---         return UpgradeBuilding.NOT_ABLE_TO_UPGRADE.RESOURCE_NOT_ENOUGTH
---     end
--- end
-
+function ResourceUpgradeBuilding:IsAbleToUpgrade(isUpgradeNow)
+    ResourceUpgradeBuilding.super.IsAbleToUpgrade(self,isUpgradeNow)
+    -- 升级是否使空闲城民小于0
+    local resource_manager = City:GetResourceManager()
+    local free_citizen = resource_manager:GetPopulationResource():GetNoneAllocatedByTime(app.timer:GetServerTime())
+    if self:GetNextLevelCitizen()>free_citizen then
+        return UpgradeBuilding.NOT_ABLE_TO_UPGRADE.FREE_CITIZEN_ERROR
+    end
+end
 return ResourceUpgradeBuilding
