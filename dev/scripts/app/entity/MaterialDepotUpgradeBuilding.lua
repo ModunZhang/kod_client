b1f91487c7f87d5b66d912ff5bcc4563704a8091
gaozhou <gaozhou.f@qq.com>
2015-01-16 11:23:37 +0800

    修改bug

--- dev/scripts/app/entity/MaterialDepotUpgradeBuilding.lua
+++ dev/scripts/app/entity/MaterialDepotUpgradeBuilding.lua
@@ -8,23 +8,23 @@ function MaterialDepotUpgradeBuilding:ctor(building_info)
 end
 function MaterialDepotUpgradeBuilding:GetNextLevelMaxMaterial()
     local level = self:GetNextLevel()
-    return config_function[level].maxMaterial
+    return level == 0 and 0 or config_function[level].maxMaterial
 end
 function MaterialDepotUpgradeBuilding:GetMaxMaterial()
     local level = self:GetLevel()
-    return config_function[level].maxMaterial
+    return level == 0 and 0 or config_function[level].maxMaterial
 end
 function MaterialDepotUpgradeBuilding:GetMaxSoldierMaterial()
     local level = self:GetLevel()
-    return config_function[level].maxSoldierMaterial
+    return level == 0 and 0 or config_function[level].maxSoldierMaterial
 end
 function MaterialDepotUpgradeBuilding:GetMaxDragonMaterial()
     local level = self:GetLevel()
-    return config_function[level].maxDragonMaterial
+    return level == 0 and 0 or config_function[level].maxDragonMaterial
 end
 function MaterialDepotUpgradeBuilding:GetMaxDragonEquipment()
     local level = self:GetLevel()
-    return config_function[level].maxDragonEquipment
+    return level == 0 and 0 or config_function[level].maxDragonEquipment
 end
 return MaterialDepotUpgradeBuilding
 
