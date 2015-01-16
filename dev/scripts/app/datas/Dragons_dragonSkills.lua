8f06770c490573147d49b45c987f3fd845158b4f
DannyHe <dannyjiajia@gmail.com>
2015-01-16 10:50:38 +0800

    生成新的配置文件

---
+++ dev/scripts/app/datas/Dragons_dragonSkills.lua
@@ -0,0 +1,114 @@
+local dragonSkills = GameDatas.Dragons.dragonSkills
+
+dragonSkills["infantryEnhance"] = {
+	["dragonSkill"] = "infantryEnhance",
+	["unlockStar"] = 1,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 30,
+	["heroBloodCostPerLevel"] = 360,
+	["effection"] = 0.010000
+}
+dragonSkills["dragonBlood"] = {
+	["dragonSkill"] = "dragonBlood",
+	["unlockStar"] = 1,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 50,
+	["heroBloodCostPerLevel"] = 120,
+	["effection"] = 0.010000
+}
+dragonSkills["dragonBreath"] = {
+	["dragonSkill"] = "dragonBreath",
+	["unlockStar"] = 2,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 50,
+	["heroBloodCostPerLevel"] = 500,
+	["effection"] = 0.010000
+}
+dragonSkills["siegeEnhance"] = {
+	["dragonSkill"] = "siegeEnhance",
+	["unlockStar"] = 3,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 30,
+	["heroBloodCostPerLevel"] = 1000,
+	["effection"] = 0.010000
+}
+dragonSkills["cavalryEnhance"] = {
+	["dragonSkill"] = "cavalryEnhance",
+	["unlockStar"] = 3,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 30,
+	["heroBloodCostPerLevel"] = 1000,
+	["effection"] = 0.010000
+}
+dragonSkills["hunterEnhance"] = {
+	["dragonSkill"] = "hunterEnhance",
+	["unlockStar"] = 3,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 30,
+	["heroBloodCostPerLevel"] = 1000,
+	["effection"] = 0.010000
+}
+dragonSkills["rapidity"] = {
+	["dragonSkill"] = "rapidity",
+	["unlockStar"] = 4,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 30,
+	["heroBloodCostPerLevel"] = 1200,
+	["effection"] = 0.010000
+}
+dragonSkills["frenzied"] = {
+	["dragonSkill"] = "frenzied",
+	["unlockStar"] = 4,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 30,
+	["heroBloodCostPerLevel"] = 2400,
+	["effection"] = 0.010000
+}
+dragonSkills["insensitive"] = {
+	["dragonSkill"] = "insensitive",
+	["unlockStar"] = 4,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 30,
+	["heroBloodCostPerLevel"] = 2400,
+	["effection"] = 0.010000
+}
+dragonSkills["leadership"] = {
+	["dragonSkill"] = "leadership",
+	["unlockStar"] = 5,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 20,
+	["heroBloodCostPerLevel"] = 3600,
+	["effection"] = 0.010000
+}
+dragonSkills["earthquake"] = {
+	["dragonSkill"] = "earthquake",
+	["unlockStar"] = 5,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 20,
+	["heroBloodCostPerLevel"] = 7200,
+	["effection"] = 0.010000
+}
+dragonSkills["greedy"] = {
+	["dragonSkill"] = "greedy",
+	["unlockStar"] = 5,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 20,
+	["heroBloodCostPerLevel"] = 7200,
+	["effection"] = 0.010000
+}
+dragonSkills["scheme"] = {
+	["dragonSkill"] = "scheme",
+	["unlockStar"] = 5,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 20,
+	["heroBloodCostPerLevel"] = 9600,
+	["effection"] = 0.010000
+}
+dragonSkills["recover"] = {
+	["dragonSkill"] = "recover",
+	["unlockStar"] = 5,
+	["IntiLevel"] = 0,
+	["maxLevel"] = 20,
+	["heroBloodCostPerLevel"] = 9600,
+	["effection"] = 0.010000
+}
