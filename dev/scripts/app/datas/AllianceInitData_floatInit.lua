4f3269c58a147881c1d3bbef510756c226f9a80a
DannyHe <dannyjiajia@gmail.com>
2015-02-02 09:21:45 +0800

    更新生成新的配置表

---
+++ dev/scripts/app/datas/AllianceInitData_floatInit.lua
@@ -0,0 +1,18 @@
+local floatInit = GameDatas.AllianceInitData.floatInit
+
+floatInit["dragonExpByKilledCitizen"] = {
+	["type"] = "dragonExpByKilledCitizen",
+	["value"] = 0.500000
+}
+floatInit["bloodByKilledCitizen"] = {
+	["type"] = "bloodByKilledCitizen",
+	["value"] = 0.500000
+}
+floatInit["killScorePerWallHp"] = {
+	["type"] = "killScorePerWallHp",
+	["value"] = 1.000000
+}
+floatInit["playerHatchDragonNeedHours"] = {
+	["type"] = "playerHatchDragonNeedHours",
+	["value"] = 0.100000
+}
---
+++ dev/scripts/app/datas/BuildingFunction_hospital.lua
@@ -0,0 +1,202 @@
+local hospital = GameDatas.BuildingFunction.hospital
+
+hospital[1] = {
+	["level"] = 1,
+	["maxCitizen"] = 3600,
+	["power"] = 120
+}
+hospital[2] = {
+	["level"] = 2,
+	["maxCitizen"] = 6000,
+	["power"] = 360
+}
+hospital[3] = {
+	["level"] = 3,
+	["maxCitizen"] = 12000,
+	["power"] = 600
+}
+hospital[4] = {
+	["level"] = 4,
+	["maxCitizen"] = 18000,
+	["power"] = 840
+}
+hospital[5] = {
+	["level"] = 5,
+	["maxCitizen"] = 24000,
+	["power"] = 1080
+}
+hospital[6] = {
+	["level"] = 6,
+	["maxCitizen"] = 30000,
+	["power"] = 1320
+}
+hospital[7] = {
+	["level"] = 7,
+	["maxCitizen"] = 36000,
+	["power"] = 1560
+}
+hospital[8] = {
+	["level"] = 8,
+	["maxCitizen"] = 42000,
+	["power"] = 1800
+}
+hospital[9] = {
+	["level"] = 9,
+	["maxCitizen"] = 48000,
+	["power"] = 2040
+}
+hospital[10] = {
+	["level"] = 10,
+	["maxCitizen"] = 54000,
+	["power"] = 2280
+}
+hospital[11] = {
+	["level"] = 11,
+	["maxCitizen"] = 60000,
+	["power"] = 2520
+}
+hospital[12] = {
+	["level"] = 12,
+	["maxCitizen"] = 66000,
+	["power"] = 2760
+}
+hospital[13] = {
+	["level"] = 13,
+	["maxCitizen"] = 72000,
+	["power"] = 3000
+}
+hospital[14] = {
+	["level"] = 14,
+	["maxCitizen"] = 78000,
+	["power"] = 3240
+}
+hospital[15] = {
+	["level"] = 15,
+	["maxCitizen"] = 84000,
+	["power"] = 3480
+}
+hospital[16] = {
+	["level"] = 16,
+	["maxCitizen"] = 90000,
+	["power"] = 3720
+}
+hospital[17] = {
+	["level"] = 17,
+	["maxCitizen"] = 96000,
+	["power"] = 3960
+}
+hospital[18] = {
+	["level"] = 18,
+	["maxCitizen"] = 102000,
+	["power"] = 4200
+}
+hospital[19] = {
+	["level"] = 19,
+	["maxCitizen"] = 108000,
+	["power"] = 4440
+}
+hospital[20] = {
+	["level"] = 20,
+	["maxCitizen"] = 114000,
+	["power"] = 4680
+}
+hospital[21] = {
+	["level"] = 21,
+	["maxCitizen"] = 120000,
+	["power"] = 4920
+}
+hospital[22] = {
+	["level"] = 22,
+	["maxCitizen"] = 126000,
+	["power"] = 5160
+}
+hospital[23] = {
+	["level"] = 23,
+	["maxCitizen"] = 132000,
+	["power"] = 5400
+}
+hospital[24] = {
+	["level"] = 24,
+	["maxCitizen"] = 138000,
+	["power"] = 5640
+}
+hospital[25] = {
+	["level"] = 25,
+	["maxCitizen"] = 144000,
+	["power"] = 5880
+}
+hospital[26] = {
+	["level"] = 26,
+	["maxCitizen"] = 150000,
+	["power"] = 6120
+}
+hospital[27] = {
+	["level"] = 27,
+	["maxCitizen"] = 156000,
+	["power"] = 6360
+}
+hospital[28] = {
+	["level"] = 28,
+	["maxCitizen"] = 162000,
+	["power"] = 6600
+}
+hospital[29] = {
+	["level"] = 29,
+	["maxCitizen"] = 168000,
+	["power"] = 6840
+}
+hospital[30] = {
+	["level"] = 30,
+	["maxCitizen"] = 174000,
+	["power"] = 7080
+}
+hospital[31] = {
+	["level"] = 31,
+	["maxCitizen"] = 180000,
+	["power"] = 7320
+}
+hospital[32] = {
+	["level"] = 32,
+	["maxCitizen"] = 186000,
+	["power"] = 7560
+}
+hospital[33] = {
+	["level"] = 33,
+	["maxCitizen"] = 192000,
+	["power"] = 7800
+}
+hospital[34] = {
+	["level"] = 34,
+	["maxCitizen"] = 198000,
+	["power"] = 8040
+}
+hospital[35] = {
+	["level"] = 35,
+	["maxCitizen"] = 204000,
+	["power"] = 8280
+}
+hospital[36] = {
+	["level"] = 36,
+	["maxCitizen"] = 210000,
+	["power"] = 8520
+}
+hospital[37] = {
+	["level"] = 37,
+	["maxCitizen"] = 216000,
+	["power"] = 8760
+}
+hospital[38] = {
+	["level"] = 38,
+	["maxCitizen"] = 222000,
+	["power"] = 9000
+}
+hospital[39] = {
+	["level"] = 39,
+	["maxCitizen"] = 228000,
+	["power"] = 9240
+}
+hospital[40] = {
+	["level"] = 40,
+	["maxCitizen"] = 234000,
+	["power"] = 9480
+}
