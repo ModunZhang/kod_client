1ce71f1bab1eeb4a946dda5548058d67e1bfa50d
DannyHe <dannyjiajia@gmail.com>
2015-02-04 17:47:51 +0800

    城墙修改

--- dev/scripts/app/ui/GameUIWall.lua
+++ dev/scripts/app/ui/GameUIWall.lua
@@ -8,6 +8,8 @@ local WidgetPushButton = import("..widget.WidgetPushButton")
 local UIListView = import(".UIListView")
 local WidgetSelectDragon = import("..widget.WidgetSelectDragon")
 local timer = app.timer
+local WidgetUseItems = import("..widget.WidgetUseItems")
+
 function GameUIWall:ctor(city,building)
 	self.city = city
     GameUIWall.super.ctor(self,city,Localize.building_name[building:GetType()],building)
@@ -42,18 +44,29 @@ function GameUIWall:CreateMilitaryUIIf()
 	if self.military_node then return self.military_node end
 	local dragon = self:GetDragon()
 	local military_node = display.newNode():size(window.width,window.height)
-	local top_bg = WidgetUIBackGround.new({height = 332,isFrame = "yes"})
+	local top_bg = WidgetUIBackGround.new({height = 382,isFrame = "yes"})
 		:addTo(military_node)
-		:pos((window.width - 608)/2,window.height - 332 - 91)
-	local title_bar = display.newSprite("title_bar_586x34.png"):align(display.LEFT_TOP,10,312):addTo(top_bg)
+		:pos((window.width - 608)/2,window.height - 382 - 91)
+	local title_bar = display.newSprite("title_bar_586x34.png"):align(display.LEFT_TOP,10,362):addTo(top_bg)
 	UIKit:ttfLabel({
 		text = _("驻防部队"),
 		size = 22,
 		color = 0xffedae
 	}):align(display.CENTER, 293, 17):addTo(title_bar)
+	local select_button = WidgetPushButton.new({
+			normal = "yellow_btn_up_148x58.png",
+			pressed = "yellow_btn_down_148x58.png",
+			disabled = "gray_btn_148x58.png"
+		})
+		:addTo(top_bg)
+		:align(display.CENTER_BOTTOM, 304,20)
+		:setButtonLabel("normal", UIKit:ttfLabel({text = _("选择")}))
+		:onButtonClicked(function()
+			self:OnSelectDragonButtonClicked()
+	end)
 	local list_bg = display.newScale9Sprite("box_bg_546x214.png")
 			:addTo(top_bg)
-			:align(display.LEFT_BOTTOM,22,30)
+			:align(display.LEFT_BOTTOM,22,80)
 			:size(568, 100)
 	self.info_list = UIListView.new({
 			bgColor = UIKit:hex2c4b(0x7a000000),
@@ -62,10 +75,10 @@ function GameUIWall:CreateMilitaryUIIf()
 		}):addTo(list_bg)
 	
 	local tips_panel = self:GetTipsBoxWithTipsContent({
-			_("・防御敌方进攻时，可能会损失城墙的生命值。"),
-			_("・防御敌方进攻时，可能会损失城墙的生命值。"),
-			_("・防御敌方进攻时，可能会损失城墙的生命值。")
-		}):addTo(top_bg):align(display.LEFT_BOTTOM,22,30)
+			_("・不驻防巨龙，所有城市中的部队将不会进行防御。"),
+			_("・驻防巨龙能在敌方进攻时，自动带兵参战。"),
+			_("・驻防巨龙能防御敌方进行的突袭，获得城市的信息")
+		}):addTo(top_bg):align(display.LEFT_BOTTOM,28,80)
 	if dragon then
 		tips_panel:hide()
 	else
@@ -75,7 +88,7 @@ function GameUIWall:CreateMilitaryUIIf()
 	self.dragon_info_panel = list_bg
 	local draogn_box = display.newSprite("alliance_item_flag_box_126X126.png")
 		:addTo(top_bg)
-		:align(display.LEFT_BOTTOM, list_bg:getPositionX(), list_bg:getPositionY()+list_bg:getContentSize().height + 10)
+		:align(display.LEFT_BOTTOM, list_bg:getPositionX(), list_bg:getPositionY()+list_bg:getContentSize().height + 12)
 	local dragon_bg = display.newSprite("dragon_bg_114x114.png", 63, 63):addTo(draogn_box)
 	self.dragon_head = display.newSprite(UILib.dragon_head['redDragon']):addTo(dragon_bg):pos(57,60)
 	if not dragon then
@@ -83,22 +96,12 @@ function GameUIWall:CreateMilitaryUIIf()
 	else
 		self.dragon_head:setTexture(UILib.dragon_head[dragon:Type()])
 	end
-	local select_button = WidgetPushButton.new({
-			normal = "yellow_btn_up_148x58.png",
-			pressed = "yellow_btn_down_148x58.png",
-			disabled = "gray_btn_148x58.png"
-		})
-		:addTo(top_bg)
-		:align(display.RIGHT_TOP, title_bar:getPositionX()+title_bar:getContentSize().width,draogn_box:getPositionY()+draogn_box:getContentSize().height)
-		:setButtonLabel("normal", UIKit:ttfLabel({text = _("选择")}))
-		:onButtonClicked(function()
-			self:OnSelectDragonButtonClicked()
-		end)
 	local progressTimer_bg = display.newSprite("process_bar_410x40.png")
-		:align(display.LEFT_BOTTOM, draogn_box:getPositionX()+draogn_box:getContentSize().width + 30, draogn_box:getPositionY()+20)
+		:align(display.LEFT_BOTTOM, draogn_box:getPositionX()+draogn_box:getContentSize().width + 30, draogn_box:getPositionY()+10)
 		:addTo(top_bg)
 	local progressTimer = UIKit:commonProgressTimer("bar_color_410x40.png"):addTo(progressTimer_bg):align(display.LEFT_BOTTOM,0,0)
 	self.dragon_hp_progress = progressTimer
+	self.progressTimer_bg = progressTimer_bg
 	local iconbg = display.newSprite("drgon_process_icon_bg.png")
      		:addTo(progressTimer_bg)
      		:align(display.LEFT_BOTTOM, -15,0)
@@ -108,42 +111,67 @@ function GameUIWall:CreateMilitaryUIIf()
     self.hp_label = UIKit:ttfLabel({
     	text = "",
     	size = 22,
-    	color= 0xfff3c7
-   	}):align(display.LEFT_CENTER, iconbg:getPositionX()+iconbg:getContentSize().width+20,20):addTo(progressTimer_bg)
+    	color= 0xfff3c7,
+    	shadow = true,
+   	}):align(display.LEFT_CENTER, iconbg:getPositionX()+iconbg:getContentSize().width+10,20):addTo(progressTimer_bg)
+ 	self.dragon_hp_recovery_label = UIKit:ttfLabel({
+ 		 text = self:GetDragonHPRecoveryStr(),
+ 		 color = 0xfff3c7,
+ 		 shadow = true,
+ 		 size = 22,
+ 	}):addTo(progressTimer_bg):align(display.RIGHT_CENTER, progressTimer_bg:getContentSize().width - 70, 20)
     if dragon then
     	progressTimer:setPercentage(dragon:Hp()/dragon:GetMaxHP()*100)
     	self.hp_label:setString(dragon:Hp() .. "/" .. dragon:GetMaxHP())
     else
     	progressTimer:setPercentage(0)
     	self.hp_label:hide()
+    	self.progressTimer_bg:hide()
     end
 
    	local button = WidgetPushButton.new({normal = 'add_button_normal_50x50.png',pressed = 'add_button_light_50x50.png'})
    		:addTo(progressTimer_bg):align(display.RIGHT_CENTER, 410, 20)
-   	local lv_str,strength = _("请选择一个巨龙驻防"),0
+   		:onButtonClicked(handler(self, self.OnDragonHpItemUseButtonClicked))
+
+   	local tips_label = UIKit:ttfLabel({
+		text = self:GetDefenceDragonBuffDesc(),
+		color= 0x514d3e,
+		size = 20 
+	}):align(display.LEFT_BOTTOM,draogn_box:getPositionX()+draogn_box:getContentSize().width + 30,  draogn_box:getPositionY()+10):addTo(top_bg)
+   	self.tips_label = tips_label
+   	local name_str = _("请选择一个巨龙驻防")
+   	--TODO:
+   	local level_str = string.format(_("当前城市地形:%s"),Localize.terrain[User:Terrain()])
    	if dragon then
-   		lv_str = Localize.dragon[dragon:Type()] .. " ( LV " .. dragon:Level() .. " )"
-   		strength = dragon:Strength()
+   		name_str = dragon:GetLocalizedName()
+   		level_str=  _("等级")
    	end
-	local lv_label = UIKit:ttfLabel({
-		text = lv_str,
+	local name_label = UIKit:ttfLabel({
+		text = name_str,
 		size = 22,
 		color= 0x514d3e
 		})
 		:addTo(top_bg)
-		:align(display.LEFT_TOP,draogn_box:getPositionX()+draogn_box:getContentSize().width + 30, draogn_box:getPositionY() + draogn_box:getContentSize().height)
-	self.lv_label = lv_label
-	local strength_label = UIKit:ttfLabel({
-		text = _("力量"),
+		:align(display.LEFT_TOP,tips_label:getPositionX(), draogn_box:getPositionY() + draogn_box:getContentSize().height)
+	self.name_label = name_label
+	local level_title_label = UIKit:ttfLabel({
+		text = level_str,
 		size = 20,
 		color= 0x797154
-	}):align(display.LEFT_BOTTOM,lv_label:getPositionX(), progressTimer_bg:getPositionY()+progressTimer_bg:getContentSize().height+10):addTo(top_bg)
-
-	self.dragon_strength_label = UIKit:ttfLabel({
-		text = strength,
+	}):align(display.LEFT_BOTTOM,name_label:getPositionX(), tips_label:getPositionY() + tips_label:getContentSize().height + 20):addTo(top_bg)
+	self.level_title_label = level_title_label
+	self.dragon_level_label = UIKit:ttfLabel({
+		text = "",
 		color= 0x514d3e,
 		size = 20 
-	}):addTo(top_bg):align(display.LEFT_BOTTOM,strength_label:getPositionX()+strength_label:getContentSize().width + 10, strength_label:getPositionY())
+	}):addTo(top_bg):align(display.LEFT_BOTTOM,level_title_label:getPositionX()+50, level_title_label:getPositionY())
+	if dragon then
+		self.dragon_level_label:setString(dragon:Level())
+		self.tips_label:hide()
+	else
+		self.dragon_level_label:hide()
+		self.tips_label:show()
+	end
 	--bottom
 
 	local wall_label = UIKit:ttfLabel({
@@ -168,7 +196,7 @@ function GameUIWall:CreateMilitaryUIIf()
 	}):align(display.LEFT_CENTER,50,20):addTo(process_wall_bg)
 
 	self.wall_hp_recovery_label = UIKit:ttfLabel({
-		text = "+" .. wallHpResource:GetProductionPerHour() .. "/H",
+		text = "+" .. wallHpResource:GetProductionPerHour() .. "/h",
 		size = 22,
 		color= 0xfff3c7
 	}):align(display.RIGHT_CENTER, 530, 20):addTo(process_wall_bg)
@@ -178,7 +206,7 @@ function GameUIWall:CreateMilitaryUIIf()
     display.newSprite("wall_icon_40x40.png")
      		:addTo(iconbg)
      		:pos(iconbg:getContentSize().width/2,iconbg:getContentSize().height/2)
-	local tips_bg = self:GetTipsBoxWithTipsContent({_("・防御敌方进攻时，可能会损失城墙的生命值。"),_("・防御敌方进攻时，可能会损失城墙的生命值。")})
+	local tips_bg = self:GetTipsBoxWithTipsContent({_("・防御敌方进攻时，可能会损失城墙的生命值。"),_("・当生命值为0时，地方联盟将击溃你的城市，获得额外的联盟积分。")})
 	tips_bg:align(display.CENTER_TOP,military_node:getContentSize().width/2,process_wall_bg:getPositionY()-process_wall_bg:getContentSize().height - 10):addTo(military_node)
 
 	self.military_node = military_node
@@ -186,6 +214,23 @@ function GameUIWall:CreateMilitaryUIIf()
 		self:RefreshListView()
 	end
 	return self.military_node
+end
+
+function GameUIWall:GetDefenceDragonBuffDesc()
+	return string.format(_("选择%s会获得战斗力加成"),Localize.dragon[User:GetBestDragon()])
+end
+
+function GameUIWall:GetDragonHPRecoveryStr()
+	local dragonEyrie = City:GetDragonEyrie()
+	return "+" .. dragonEyrie:GetHPRecoveryPerHour() .. "/h"
+end
+
+function GameUIWall:OnDragonHpItemUseButtonClicked()
+	local widgetUseItems = WidgetUseItems.new():Create({
+		item_type = WidgetUseItems.USE_TYPE.DRAGON_EXP,
+		dragon = self:GetDragon()
+	})
+	widgetUseItems:addToCurrentScene()
 end
 
 function GameUIWall:GetTipsBoxWithTipsContent(content)
@@ -211,8 +256,9 @@ end
 
 function GameUIWall:GetListData()
 	local troops_count = self:GetDragon():LeadCitizen()
+	local totalstrength = self:GetDragon():TotalStrength()
 	local data = {
-		{title = _("龙的经验"),val = 100,buffer = 200},
+		{title = _("龙的力量"),val = totalstrength},
 		{title = _("最大兵量"),val = troops_count},
 	}
 	return data
@@ -285,13 +331,18 @@ end
 
 function GameUIWall:OnDragonSelected(dragon)
 	if dragon then
+		if self:GetDragon() and self:GetDragon():Type() == dragon:Type() then return end
 		NetManager:getSetDefenceDragonPromise(dragon:Type()):next(function()
 			self:RefreshUIAfterSelectDragon(dragon)
 		end)
 	else
-		NetManager:getCancelDefenceDragonPromise():next(function()
-			self:RefreshUIAfterSelectDragon()
-		end)
+		if self:GetDragon() then
+			NetManager:getCancelDefenceDragonPromise():next(function()
+				self:RefreshUIAfterSelectDragon()
+			end)
+		else
+			UIKit:showMessageDialog(_("错误"),_("还没有龙驻防"), function()end)
+		end
 	end
 end
 
@@ -299,8 +350,11 @@ function GameUIWall:RefreshUIAfterSelectDragon(dragon)
 	if dragon then
 		self.dragon_info_panel:show()
 		self.tips_panel:hide()
-		self.lv_label:setString(Localize.dragon[dragon:Type()] .. " ( LV " .. dragon:Level() .. " )")
-		self.dragon_strength_label:setString(dragon:Strength())
+		self.level_title_label:setString(_("等级"))
+		self.dragon_level_label:setString(dragon:Level())
+		self.dragon_level_label:show()
+		self.tips_label:hide()
+		self.progressTimer_bg:show()
 		self.hp_label:setString(dragon:Hp() .. "/" .. dragon:GetMaxHP())
 		self.hp_label:show()
 		self.dragon_hp_progress:setPercentage(dragon:Hp()/dragon:GetMaxHP()*100)
@@ -310,8 +364,10 @@ function GameUIWall:RefreshUIAfterSelectDragon(dragon)
 	else
 		self.dragon_info_panel:hide()
 		self.tips_panel:show()
-		self.lv_label:setString(_("请选择一个巨龙驻防"))
-		self.dragon_strength_label:setString("0")
+		self.tips_label:show()
+		self.dragon_level_label:hide()
+		self.level_title_label:setString(string.format(_("当前城市地形:%s"),Localize.terrain[User:Terrain()]))
+		self.progressTimer_bg:hide()
 		self.hp_label:hide()
 		self.dragon_hp_progress:setPercentage(0)
 		self.dragon_head:hide()
