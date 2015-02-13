f25414eff249913e136ae9cdd8b60b3f885c50f6
DannyHe <dannyjiajia@gmail.com>
2015-02-13 10:18:17 +0800

    新的服务器配置文件

---
+++ dev/scripts/app/datas/ClientInitGame_pve_npc.lua
@@ -0,0 +1,67 @@
+local pve_npc = GameDatas.ClientInitGame.pve_npc
+
+pve_npc["WOODCUTTER"] = {
+	["type"] = "WOODCUTTER",
+	["rewards"] = "resources,wood,12",
+	["rewards_type"] = "all"
+}
+pve_npc["QUARRIER"] = {
+	["type"] = "QUARRIER",
+	["rewards"] = "resources,wood,13",
+	["rewards_type"] = "all"
+}
+pve_npc["MINER"] = {
+	["type"] = "MINER",
+	["rewards"] = "resources,wood,14",
+	["rewards_type"] = "all"
+}
+pve_npc["FARMER"] = {
+	["type"] = "FARMER",
+	["rewards"] = "resources,wood,15",
+	["rewards_type"] = "all"
+}
+pve_npc["CAMP"] = {
+	["type"] = "CAMP",
+	["rewards"] = "items,woodBonus_1,1,20;items,stoneBonus_1,1,20;items,ironBonus_1,1,20;items,foodBonus_1,1,20;items,coinClass_1,1,20",
+	["rewards_type"] = "random"
+}
+pve_npc["CRASHED_AIRSHIP"] = {
+	["type"] = "CRASHED_AIRSHIP",
+	["rewards"] = "items,infantryAtkBonus_1,1,20;items,archerAtkBonus_1,1,20;items,cavalryAtkBonus_1,1,20;items,siegeAtkBonus_1,1,20;items,masterOfDefender_1,1,20",
+	["rewards_type"] = "random"
+}
+pve_npc["CONSTRUCTION_RUINS"] = {
+	["type"] = "CONSTRUCTION_RUINS",
+	["rewards"] = "items,woodClass_1,1,20;items,stoneClass_1,1,20;items,ironClass_1,1,20;items,foodClass_1,1,20;items,woodClass_1,1,20",
+	["rewards_type"] = "random"
+}
+pve_npc["KEEL"] = {
+	["type"] = "KEEL",
+	["rewards"] = "items,dragonHpBonus_1,1,1;items,dragonExpBonus_1,1",
+	["rewards_type"] = "select"
+}
+pve_npc["WARRIORS_TOMB"] = {
+	["type"] = "WARRIORS_TOMB",
+	["rewards"] = "items,heroBlood_1,1",
+	["rewards_type"] = "all"
+}
+pve_npc["OBELISK"] = {
+	["type"] = "OBELISK",
+	["rewards"] = "resources,wood,21",
+	["rewards_type"] = "all"
+}
+pve_npc["ANCIENT_RUINS"] = {
+	["type"] = "ANCIENT_RUINS",
+	["rewards"] = "items,vipActive_1,18,60;items,vipActive_2,18,30;items,vipActive_3,18,10",
+	["rewards_type"] = "random"
+}
+pve_npc["ENTRANCE_DOOR"] = {
+	["type"] = "ENTRANCE_DOOR",
+	["rewards"] = "resources,wood,23",
+	["rewards_type"] = "all"
+}
+pve_npc["TRAP_MONSTER"] = {
+	["type"] = "TRAP_MONSTER",
+	["rewards"] = "resources,wood,18,60;resources,stone,18,30;resources,food,18,10",
+	["rewards_type"] = "random"
+}
