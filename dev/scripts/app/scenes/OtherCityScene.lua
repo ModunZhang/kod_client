a0127b784ec189ad3d1def53560fa480706ed906
DannyHe <dannyjiajia@gmail.com>
2015-03-05 15:05:47 +0800

    KOD-124

--- dev/scripts/app/scenes/OtherCityScene.lua
+++ dev/scripts/app/scenes/OtherCityScene.lua
@@ -10,16 +10,8 @@ function OtherCityScene:onEnter()
     OtherCityScene.super.onEnter(self)
     UIKit:newGameUI('GameUICityInfo', self.user):addToScene(self):setTouchSwallowEnabled(false)
 end
-
+--不处理任何场景建筑事件
 function OtherCityScene:OnTouchClicked(pre_x, pre_y, x, y)
-	local building = self:GetSceneLayer():GetClickedObject(x, y)
-    if building then
-        if iskindof(building, "HelpedTroopsSprite") then
-            local helped = self.city:GetHelpedByTroops()[building:GetIndex()]
-            local type_ = GameUIWatchTowerTroopDetail.DATA_TYPE.HELP_DEFENCE
-            UIKit:newGameUI("GameUIWatchTowerTroopDetail", type_, helped, self.user:Id()):addToCurrentScene(true)
-        end
-    end
 end
 
 return OtherCityScene
