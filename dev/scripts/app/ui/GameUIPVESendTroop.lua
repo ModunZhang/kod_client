c1c44e5cdd89d4b326b844f47c7f175142c60b27
Kenny Dai <daidai917@gmail.com>
2015-04-06 15:59:03 +0800

    联盟派兵界面动画位置适配

--- dev/scripts/app/ui/GameUIPVESendTroop.lua
+++ dev/scripts/app/ui/GameUIPVESendTroop.lua
@@ -90,15 +90,14 @@ function GameUIPVESendTroop:OnMoveInStage()
                             break
                         end
                     end
-                    self:RefreashSoldierShow()
                 else
                     for k,item in pairs(self.soldiers_table) do
                         local _,_,_,max_num = item:GetSoldierInfo()
                         item:SetSoldierCount(max_num)
                     end
-                    self:RefreashSoldierShow()
                 end
             end
+            self:RefreashSoldierShow()
         end
     end):align(display.LEFT_CENTER,window.left+50,window.top-910):addTo(self:GetView())
     self.max_btn = max_btn
