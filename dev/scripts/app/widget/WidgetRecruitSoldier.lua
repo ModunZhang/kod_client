35882988eddd5a00cd4815d32b26051943488e84
Kenny Dai <daidai917@gmail.com>
2015-04-07 21:17:27 +0800

    xx

--- dev/scripts/app/widget/WidgetRecruitSoldier.lua
+++ dev/scripts/app/widget/WidgetRecruitSoldier.lua
@@ -383,7 +383,7 @@ function WidgetRecruitSoldier:ctor(barracks, city, soldier_name,soldier_star)
                 local not_enough_material = self:CheckMaterials(self.count)
                 local required_gems = DataUtils:buyResource(self:GetNeedResouce(self.count), {})
                 if not_enough_material then
-                    UIKit:showMessageDialog(_("招募材料不足"), _("您当前没有足够"))
+                    UIKit:showMessageDialog(_("招募材料不足"), _("您当前没有足够材料"))
                 elseif queue_need_gem + required_gems > 0 then
                     local title = string.format("%s/%s", queue_need_gem > 0 and _("队列不足") or "", required_gems > 0 and _("资源不足") or "")
                     local content = string.format("%s%s%s", queue_need_gem > 0 and _("您当前没有足够的队列") or "", required_gems > 0 and _("您当前没有足够的资源") or "", _("是否花费魔法石立即补充"))
