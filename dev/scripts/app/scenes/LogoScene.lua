02336aa873e0a2a8a9c8b82e120f0a93f123d1cb
DannyHe <dannyjiajia@gmail.com>
2015-04-08 11:42:14 +0800

    整理代码

--- dev/scripts/app/scenes/LogoScene.lua
+++ dev/scripts/app/scenes/LogoScene.lua
@@ -6,9 +6,8 @@ require("app.ui.GameGlobalUIUtils")
 local LogoScene = class("LogoScene", function()
     return display.newScene("LogoScene")
 end)
---提前加载splash需要的资源
 function LogoScene:ctor()
-    self:PreLoadResource()
+    self:loadSplashResources()
 end
 
 function LogoScene:onEnter()
@@ -18,28 +17,21 @@ function LogoScene:onEnter()
 end
 
 function LogoScene:beginAnimate()
-    -- transition.execute(self.sprite, cc.ScaleTo:create(checknumber(2),1.5))
-    -- transition.fadeTo(self.sprite, {opacity = 255/2, time = 1.5})
     local action = cc.Spawn:create({cc.ScaleTo:create(checknumber(2),1.5),cca.fadeTo(1.5,255/2)})
     self.sprite:runAction(action)
-    -- transition.fadeOut(self.layer,{time = 2})
     local sequence = transition.sequence({
         cc.FadeOut:create(1),
         cc.CallFunc:create(function()
             self:performWithDelay(function()
                 self.sprite:removeFromParent(true)
-                if CONFIG_IS_DEBUG then
-                    app:enterScene("MainScene")
-                else
-                    -- app:enterScene("UpdaterScene")
-                end
+                app:enterScene("MainScene")
             end, 0.5)
         end),
     })
     self.layer:runAction(sequence)
 end
---TODO:预先加载登录界面使用的大图 
-function LogoScene:PreLoadResource()
+--预先加载登录界面使用的大图 
+function LogoScene:loadSplashResources()
     --加载splash界面使用的图片
     display.addImageAsync("splash_beta_logo_515x119.png",function()
         display.addImageAsync("splash_beta_bg_3987x1136.jpg",function()end)
