0179d598a4dd8c2660f5c7876c9f5949187c2a7f
gaozhou <gaozhou.f@qq.com>
2015-04-09 20:36:17 +0800

    修改数据

---
+++ dev/scripts/app/datas/AllianceInitData_buildings.lua
@@ -0,0 +1,998 @@
+local buildings = GameDatas.AllianceInitData.buildings
+
+buildings[0] = {
+	["index"] = 0,
+	["name"] = "palace",
+	["locationX"] = 20,
+	["locationY"] = 21
+}
+buildings[1] = {
+	["index"] = 1,
+	["name"] = "moonGate",
+	["locationX"] = 31,
+	["locationY"] = 25
+}
+buildings[2] = {
+	["index"] = 2,
+	["name"] = "orderHall",
+	["locationX"] = 20,
+	["locationY"] = 29
+}
+buildings[3] = {
+	["index"] = 3,
+	["name"] = "shrine",
+	["locationX"] = 28,
+	["locationY"] = 31
+}
+buildings[4] = {
+	["index"] = 4,
+	["name"] = "shop",
+	["locationX"] = 28,
+	["locationY"] = 19
+}
+buildings[5] = {
+	["index"] = 5,
+	["name"] = "decorate_lake_1",
+	["locationX"] = 26,
+	["locationY"] = 26
+}
+buildings[6] = {
+	["index"] = 6,
+	["name"] = "decorate_lake_2",
+	["locationX"] = 24,
+	["locationY"] = 3
+}
+buildings[7] = {
+	["index"] = 7,
+	["name"] = "decorate_lake_2",
+	["locationX"] = 30,
+	["locationY"] = 36
+}
+buildings[8] = {
+	["index"] = 8,
+	["name"] = "decorate_lake_2",
+	["locationX"] = 4,
+	["locationY"] = 26
+}
+buildings[9] = {
+	["index"] = 9,
+	["name"] = "decorate_mountain_1",
+	["locationX"] = 19,
+	["locationY"] = 18
+}
+buildings[10] = {
+	["index"] = 10,
+	["name"] = "decorate_mountain_1",
+	["locationX"] = 35,
+	["locationY"] = 32
+}
+buildings[11] = {
+	["index"] = 11,
+	["name"] = "decorate_mountain_2",
+	["locationX"] = 22,
+	["locationY"] = 17
+}
+buildings[12] = {
+	["index"] = 12,
+	["name"] = "decorate_mountain_2",
+	["locationX"] = 16,
+	["locationY"] = 21
+}
+buildings[13] = {
+	["index"] = 13,
+	["name"] = "decorate_mountain_2",
+	["locationX"] = 36,
+	["locationY"] = 27
+}
+buildings[14] = {
+	["index"] = 14,
+	["name"] = "decorate_mountain_2",
+	["locationX"] = 26,
+	["locationY"] = 48
+}
+buildings[15] = {
+	["index"] = 15,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 21,
+	["locationY"] = 0
+}
+buildings[16] = {
+	["index"] = 16,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 22,
+	["locationY"] = 0
+}
+buildings[17] = {
+	["index"] = 17,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 23,
+	["locationY"] = 0
+}
+buildings[18] = {
+	["index"] = 18,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 24,
+	["locationY"] = 0
+}
+buildings[19] = {
+	["index"] = 19,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 27,
+	["locationY"] = 0
+}
+buildings[20] = {
+	["index"] = 20,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 47,
+	["locationY"] = 0
+}
+buildings[21] = {
+	["index"] = 21,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 49,
+	["locationY"] = 0
+}
+buildings[22] = {
+	["index"] = 22,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 25,
+	["locationY"] = 1
+}
+buildings[23] = {
+	["index"] = 23,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 26,
+	["locationY"] = 1
+}
+buildings[24] = {
+	["index"] = 24,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 27,
+	["locationY"] = 1
+}
+buildings[25] = {
+	["index"] = 25,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 29,
+	["locationY"] = 1
+}
+buildings[26] = {
+	["index"] = 26,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 4,
+	["locationY"] = 2
+}
+buildings[27] = {
+	["index"] = 27,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 25,
+	["locationY"] = 2
+}
+buildings[28] = {
+	["index"] = 28,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 3,
+	["locationY"] = 3
+}
+buildings[29] = {
+	["index"] = 29,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 25,
+	["locationY"] = 3
+}
+buildings[30] = {
+	["index"] = 30,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 26,
+	["locationY"] = 3
+}
+buildings[31] = {
+	["index"] = 31,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 43,
+	["locationY"] = 3
+}
+buildings[32] = {
+	["index"] = 32,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 50,
+	["locationY"] = 3
+}
+buildings[33] = {
+	["index"] = 33,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 24,
+	["locationY"] = 4
+}
+buildings[34] = {
+	["index"] = 34,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 25,
+	["locationY"] = 4
+}
+buildings[35] = {
+	["index"] = 35,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 23,
+	["locationY"] = 5
+}
+buildings[36] = {
+	["index"] = 36,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 24,
+	["locationY"] = 5
+}
+buildings[37] = {
+	["index"] = 37,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 40,
+	["locationY"] = 5
+}
+buildings[38] = {
+	["index"] = 38,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 24,
+	["locationY"] = 6
+}
+buildings[39] = {
+	["index"] = 39,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 6,
+	["locationY"] = 7
+}
+buildings[40] = {
+	["index"] = 40,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 16,
+	["locationY"] = 7
+}
+buildings[41] = {
+	["index"] = 41,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 24,
+	["locationY"] = 7
+}
+buildings[42] = {
+	["index"] = 42,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 50,
+	["locationY"] = 7
+}
+buildings[43] = {
+	["index"] = 43,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 7,
+	["locationY"] = 8
+}
+buildings[44] = {
+	["index"] = 44,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 9,
+	["locationY"] = 8
+}
+buildings[45] = {
+	["index"] = 45,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 14,
+	["locationY"] = 8
+}
+buildings[46] = {
+	["index"] = 46,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 46,
+	["locationY"] = 8
+}
+buildings[47] = {
+	["index"] = 47,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 15,
+	["locationY"] = 9
+}
+buildings[48] = {
+	["index"] = 48,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 1,
+	["locationY"] = 10
+}
+buildings[49] = {
+	["index"] = 49,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 26,
+	["locationY"] = 10
+}
+buildings[50] = {
+	["index"] = 50,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 23,
+	["locationY"] = 12
+}
+buildings[51] = {
+	["index"] = 51,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 27,
+	["locationY"] = 12
+}
+buildings[52] = {
+	["index"] = 52,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 10,
+	["locationY"] = 13
+}
+buildings[53] = {
+	["index"] = 53,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 37,
+	["locationY"] = 13
+}
+buildings[54] = {
+	["index"] = 54,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 49,
+	["locationY"] = 13
+}
+buildings[55] = {
+	["index"] = 55,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 3,
+	["locationY"] = 14
+}
+buildings[56] = {
+	["index"] = 56,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 2,
+	["locationY"] = 15
+}
+buildings[57] = {
+	["index"] = 57,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 14,
+	["locationY"] = 16
+}
+buildings[58] = {
+	["index"] = 58,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 23,
+	["locationY"] = 16
+}
+buildings[59] = {
+	["index"] = 59,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 35,
+	["locationY"] = 16
+}
+buildings[60] = {
+	["index"] = 60,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 23,
+	["locationY"] = 17
+}
+buildings[61] = {
+	["index"] = 61,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 45,
+	["locationY"] = 17
+}
+buildings[62] = {
+	["index"] = 62,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 4,
+	["locationY"] = 18
+}
+buildings[63] = {
+	["index"] = 63,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 21,
+	["locationY"] = 18
+}
+buildings[64] = {
+	["index"] = 64,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 22,
+	["locationY"] = 18
+}
+buildings[65] = {
+	["index"] = 65,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 21,
+	["locationY"] = 19
+}
+buildings[66] = {
+	["index"] = 66,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 26,
+	["locationY"] = 20
+}
+buildings[67] = {
+	["index"] = 67,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 25,
+	["locationY"] = 21
+}
+buildings[68] = {
+	["index"] = 68,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 26,
+	["locationY"] = 21
+}
+buildings[69] = {
+	["index"] = 69,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 50,
+	["locationY"] = 21
+}
+buildings[70] = {
+	["index"] = 70,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 0,
+	["locationY"] = 22
+}
+buildings[71] = {
+	["index"] = 71,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 4,
+	["locationY"] = 22
+}
+buildings[72] = {
+	["index"] = 72,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 7,
+	["locationY"] = 22
+}
+buildings[73] = {
+	["index"] = 73,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 0,
+	["locationY"] = 23
+}
+buildings[74] = {
+	["index"] = 74,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 3,
+	["locationY"] = 23
+}
+buildings[75] = {
+	["index"] = 75,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 8,
+	["locationY"] = 23
+}
+buildings[76] = {
+	["index"] = 76,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 50,
+	["locationY"] = 23
+}
+buildings[77] = {
+	["index"] = 77,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 1,
+	["locationY"] = 24
+}
+buildings[78] = {
+	["index"] = 78,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 5,
+	["locationY"] = 24
+}
+buildings[79] = {
+	["index"] = 79,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 7,
+	["locationY"] = 24
+}
+buildings[80] = {
+	["index"] = 80,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 8,
+	["locationY"] = 24
+}
+buildings[81] = {
+	["index"] = 81,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 9,
+	["locationY"] = 24
+}
+buildings[82] = {
+	["index"] = 82,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 27,
+	["locationY"] = 24
+}
+buildings[83] = {
+	["index"] = 83,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 28,
+	["locationY"] = 24
+}
+buildings[84] = {
+	["index"] = 84,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 34,
+	["locationY"] = 24
+}
+buildings[85] = {
+	["index"] = 85,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 36,
+	["locationY"] = 24
+}
+buildings[86] = {
+	["index"] = 86,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 43,
+	["locationY"] = 24
+}
+buildings[87] = {
+	["index"] = 87,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 45,
+	["locationY"] = 24
+}
+buildings[88] = {
+	["index"] = 88,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 45,
+	["locationY"] = 24
+}
+buildings[89] = {
+	["index"] = 89,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 46,
+	["locationY"] = 24
+}
+buildings[90] = {
+	["index"] = 90,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 49,
+	["locationY"] = 24
+}
+buildings[91] = {
+	["index"] = 91,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 50,
+	["locationY"] = 24
+}
+buildings[92] = {
+	["index"] = 92,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 1,
+	["locationY"] = 25
+}
+buildings[93] = {
+	["index"] = 93,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 5,
+	["locationY"] = 25
+}
+buildings[94] = {
+	["index"] = 94,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 27,
+	["locationY"] = 25
+}
+buildings[95] = {
+	["index"] = 95,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 37,
+	["locationY"] = 25
+}
+buildings[96] = {
+	["index"] = 96,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 44,
+	["locationY"] = 25
+}
+buildings[97] = {
+	["index"] = 97,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 45,
+	["locationY"] = 25
+}
+buildings[98] = {
+	["index"] = 98,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 47,
+	["locationY"] = 25
+}
+buildings[99] = {
+	["index"] = 99,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 48,
+	["locationY"] = 25
+}
+buildings[100] = {
+	["index"] = 100,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 50,
+	["locationY"] = 25
+}
+buildings[101] = {
+	["index"] = 101,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 0,
+	["locationY"] = 26
+}
+buildings[102] = {
+	["index"] = 102,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 17,
+	["locationY"] = 26
+}
+buildings[103] = {
+	["index"] = 103,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 38,
+	["locationY"] = 26
+}
+buildings[104] = {
+	["index"] = 104,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 46,
+	["locationY"] = 26
+}
+buildings[105] = {
+	["index"] = 105,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 50,
+	["locationY"] = 26
+}
+buildings[106] = {
+	["index"] = 106,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 0,
+	["locationY"] = 27
+}
+buildings[107] = {
+	["index"] = 107,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 1,
+	["locationY"] = 27
+}
+buildings[108] = {
+	["index"] = 108,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 17,
+	["locationY"] = 27
+}
+buildings[109] = {
+	["index"] = 109,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 22,
+	["locationY"] = 27
+}
+buildings[110] = {
+	["index"] = 110,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 23,
+	["locationY"] = 27
+}
+buildings[111] = {
+	["index"] = 111,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 44,
+	["locationY"] = 27
+}
+buildings[112] = {
+	["index"] = 112,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 49,
+	["locationY"] = 27
+}
+buildings[113] = {
+	["index"] = 113,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 0,
+	["locationY"] = 28
+}
+buildings[114] = {
+	["index"] = 114,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 22,
+	["locationY"] = 28
+}
+buildings[115] = {
+	["index"] = 115,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 48,
+	["locationY"] = 28
+}
+buildings[116] = {
+	["index"] = 116,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 50,
+	["locationY"] = 28
+}
+buildings[117] = {
+	["index"] = 117,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 4,
+	["locationY"] = 30
+}
+buildings[118] = {
+	["index"] = 118,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 6,
+	["locationY"] = 30
+}
+buildings[119] = {
+	["index"] = 119,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 36,
+	["locationY"] = 30
+}
+buildings[120] = {
+	["index"] = 120,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 5,
+	["locationY"] = 31
+}
+buildings[121] = {
+	["index"] = 121,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 36,
+	["locationY"] = 31
+}
+buildings[122] = {
+	["index"] = 122,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 11,
+	["locationY"] = 32
+}
+buildings[123] = {
+	["index"] = 123,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 45,
+	["locationY"] = 32
+}
+buildings[124] = {
+	["index"] = 124,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 12,
+	["locationY"] = 33
+}
+buildings[125] = {
+	["index"] = 125,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 30,
+	["locationY"] = 33
+}
+buildings[126] = {
+	["index"] = 126,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 31,
+	["locationY"] = 33
+}
+buildings[127] = {
+	["index"] = 127,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 32,
+	["locationY"] = 33
+}
+buildings[128] = {
+	["index"] = 128,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 31,
+	["locationY"] = 34
+}
+buildings[129] = {
+	["index"] = 129,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 41,
+	["locationY"] = 34
+}
+buildings[130] = {
+	["index"] = 130,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 7,
+	["locationY"] = 36
+}
+buildings[131] = {
+	["index"] = 131,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 19,
+	["locationY"] = 36
+}
+buildings[132] = {
+	["index"] = 132,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 22,
+	["locationY"] = 36
+}
+buildings[133] = {
+	["index"] = 133,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 21,
+	["locationY"] = 37
+}
+buildings[134] = {
+	["index"] = 134,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 35,
+	["locationY"] = 37
+}
+buildings[135] = {
+	["index"] = 135,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 22,
+	["locationY"] = 38
+}
+buildings[136] = {
+	["index"] = 136,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 47,
+	["locationY"] = 38
+}
+buildings[137] = {
+	["index"] = 137,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 13,
+	["locationY"] = 39
+}
+buildings[138] = {
+	["index"] = 138,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 42,
+	["locationY"] = 40
+}
+buildings[139] = {
+	["index"] = 139,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 3,
+	["locationY"] = 41
+}
+buildings[140] = {
+	["index"] = 140,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 10,
+	["locationY"] = 41
+}
+buildings[141] = {
+	["index"] = 141,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 9,
+	["locationY"] = 42
+}
+buildings[142] = {
+	["index"] = 142,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 25,
+	["locationY"] = 43
+}
+buildings[143] = {
+	["index"] = 143,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 26,
+	["locationY"] = 43
+}
+buildings[144] = {
+	["index"] = 144,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 24,
+	["locationY"] = 44
+}
+buildings[145] = {
+	["index"] = 145,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 38,
+	["locationY"] = 44
+}
+buildings[146] = {
+	["index"] = 146,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 44,
+	["locationY"] = 44
+}
+buildings[147] = {
+	["index"] = 147,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 50,
+	["locationY"] = 44
+}
+buildings[148] = {
+	["index"] = 148,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 20,
+	["locationY"] = 45
+}
+buildings[149] = {
+	["index"] = 149,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 27,
+	["locationY"] = 45
+}
+buildings[150] = {
+	["index"] = 150,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 0,
+	["locationY"] = 46
+}
+buildings[151] = {
+	["index"] = 151,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 6,
+	["locationY"] = 46
+}
+buildings[152] = {
+	["index"] = 152,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 19,
+	["locationY"] = 46
+}
+buildings[153] = {
+	["index"] = 153,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 28,
+	["locationY"] = 46
+}
+buildings[154] = {
+	["index"] = 154,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 32,
+	["locationY"] = 46
+}
+buildings[155] = {
+	["index"] = 155,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 41,
+	["locationY"] = 47
+}
+buildings[156] = {
+	["index"] = 156,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 0,
+	["locationY"] = 49
+}
+buildings[157] = {
+	["index"] = 157,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 2,
+	["locationY"] = 49
+}
+buildings[158] = {
+	["index"] = 158,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 20,
+	["locationY"] = 49
+}
+buildings[159] = {
+	["index"] = 159,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 24,
+	["locationY"] = 49
+}
+buildings[160] = {
+	["index"] = 160,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 25,
+	["locationY"] = 49
+}
+buildings[161] = {
+	["index"] = 161,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 29,
+	["locationY"] = 49
+}
+buildings[162] = {
+	["index"] = 162,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 50,
+	["locationY"] = 49
+}
+buildings[163] = {
+	["index"] = 163,
+	["name"] = "decorate_tree_1",
+	["locationX"] = 3,
+	["locationY"] = 50
+}
+buildings[164] = {
+	["index"] = 164,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 27,
+	["locationY"] = 50
+}
+buildings[165] = {
+	["index"] = 165,
+	["name"] = "decorate_tree_2",
+	["locationX"] = 48,
+	["locationY"] = 50
+}
