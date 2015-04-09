43aa9c19741ddef568c728ef28eb72383c414b3e
gaozhou <gaozhou.f@qq.com>
2015-04-09 20:34:23 +0800

    提交地图生成脚本

---
+++ tools/build_map.lua
@@ -0,0 +1,41 @@
+local args = {...}
+
+local map = dofile(args[1])
+
+
+local buildings_map = {
+	"palace",
+	"orderHall",
+	"shrine",
+	"shop",
+	"moonGate",
+	"decorate_mountain_1",
+	"decorate_mountain_2",
+	"decorate_lake_1",
+	"decorate_lake_2",
+	"decorate_tree_1",
+	"decorate_tree_2",
+	"decorate_tree_3",
+	"decorate_tree_4",
+}
+
+
+for i,layer in ipairs(map.layers) do
+    if layer.name == "buildings" then
+        local min = 999
+        for i,v in ipairs(layer.data) do
+            if v ~= 0 and v < min then
+            	min = v
+            end
+        end
+        for i = 1, #layer.data do
+        	layer.data[i] = layer.data[i] - min + 1
+        end
+        for i,v in ipairs(layer.data) do
+        	if v > 0 then
+        		local index = (i-1)
+        		print(buildings_map[v], index % layer.width, math.floor(index / layer.width))
+        	end
+        end
+    end
+end
