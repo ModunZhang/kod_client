8a5e2516d6025141b638b466c84aaaf1064a1e52
gaozhou <gaozhou.f@qq.com>
2015-05-03 20:21:43 +0800

    修改bug

--- dev/scripts/app/entity/Observer.lua
+++ dev/scripts/app/entity/Observer.lua
@@ -34,18 +34,15 @@ function Observer:RemoveAllObserver()
 	self.observer = {}
 end
 function Observer:RemoveObserver(observer)
-	for i, v in ipairs(self.observer) do
+	for i,v in ipairs(self.observer) do
 		if v == observer then
 			return table.remove(self.observer, i)
 		end
 	end
 end
 function Observer:NotifyObservers(func)
-	for i, v in ipairs(self.observer) do
-		if type(v) == nil then table.remove(self.observer,i) return end
-		if func(v) then
-			return
-		end
+	for _,v in pairs(self.observer) do
+		if func(v) then return end
 	end
 end
 
