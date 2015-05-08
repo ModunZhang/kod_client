6fd3b083f3bfe3f170644bd1c088ebfe7e052da2
Kenny Dai <daidai917@gmail.com>
2015-05-08 15:27:46 +0800

    gacha数量

--- dev/scripts/app/ui/GameUIGacha.lua
+++ dev/scripts/app/ui/GameUIGacha.lua
@@ -230,7 +230,7 @@ function GameUIGacha:CreateGachaPool(layer)
         local award = display.newScale9Sprite(draw_item_box:GetGachaItemIcon()):addTo(layer,2)
             :align(display.CENTER, draw_item_box:getPositionX()-draw_item_box:getContentSize().width/2, draw_item_box:getPositionY()-draw_item_box:getContentSize().height/2)
         award:scale(74/award:getContentSize().width)
-
+        UIKit:addTipsToNode( award,Localize_item.item_name[draw_item_box:GetGachaItemName()] , main)
         local gacha_box = self.continuous_draw_items and gacha_boxes[self.continuous_index-1] or gacha_boxes[1]
         -- local gacha_box_lable
 
