4518d9874e86fdeb82a33ccc70b0a844b3abaafb
gaozhou <gaozhou.f@qq.com>
2015-05-10 20:50:25 +0800

    修改bug

--- dev/scripts/app/fte/check.lua
+++ dev/scripts/app/fte/check.lua
@@ -4,7 +4,7 @@ return function(key)
     end
     local real_key = DataManager:getUserData()._id.."_"..key
     local has_key = cc.UserDefault:getInstance():getBoolForKey(real_key)
-    print_(string.format("%s.%40s : %s", DataManager:getUserData()._id, key, has_key and "true" or "false"))
+    print(string.format("%s.%40s : %s", DataManager:getUserData()._id, key, has_key and "true" or "false"))
     return has_key
 end
 
