da34522d7d8caba734098ebc0cf07b9738311e67
Kenny Dai <daidai917@gmail.com>
2015-05-11 11:20:22 +0800

    xx

--- dev/scripts/app/service/NetManager.lua
+++ dev/scripts/app/service/NetManager.lua
@@ -461,7 +461,7 @@ end
 -- 重写OpenUDID
 local getOpenUDID = device.getOpenUDID
 device.getOpenUDID = function()
-    if true then return "1_0" end
+    -- if true then return "1_0" end
     if CONFIG_IS_DEBUG then
         local device_id
         local udid = cc.UserDefault:getInstance():getStringForKey("udid")
