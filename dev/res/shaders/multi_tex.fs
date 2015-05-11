b565d2559faca91574068c5cb01a7287f4576864
gaozhou <gaozhou.f@qq.com>
2015-05-11 22:38:13 +0800

    优化行军路线

--- dev/res/shaders/multi_tex.fs
+++ dev/res/shaders/multi_tex.fs
@@ -10,8 +10,11 @@ uniform float percent;
 uniform float elapse;
 void main(void)
 {
-	float p = smoothstep(elapse - 0.1, elapse, 1.0 - v_texCoord.y);
+	if ( elapse > 1.0 - v_texCoord.y ){
+		discard;
+	}
+	// float p = smoothstep(elapse - 0.1, elapse, 1.0 - v_texCoord.y);
 	float y = mod( v_texCoord.y, unit_len ) * unit_count;
 	float ry = mod( y + percent, 1.0 );
-	gl_FragColor = texture2D( CC_Texture0, vec2( v_texCoord.x, ry ) ) * p;
+	gl_FragColor = texture2D( CC_Texture0, vec2( v_texCoord.x, ry ) );
 }
