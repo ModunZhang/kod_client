87cbad2e8f13fa1cc5d05099f8e2f2779612857b
gaozhou <gaozhou.f@qq.com>
2015-05-15 21:04:28 +0800

    修改bug

--- dev/scripts/app/scenes/FriendCityScene.lua
+++ dev/scripts/app/scenes/FriendCityScene.lua
@@ -15,26 +15,9 @@ end
 function FriendCityScene:OnTouchClicked(pre_x, pre_y, x, y)
     local building = self:GetSceneLayer():GetClickedObject(x, y)
     if building then
-        local buildings = {}
-        if building:GetEntity():GetType() == "wall" then
-            for i,v in ipairs(self:GetSceneLayer():GetWalls()) do
-                table.insert(buildings, v)
-            end
-            for i,v in ipairs(self:GetSceneLayer():GetTowers()) do
-                table.insert(buildings, v)
-            end
-        elseif building:GetEntity():GetType() == "tower" then
-            buildings = {unpack(self:GetSceneLayer():GetTowers())}
-        else
-            buildings = {building}
-        end
+        app:lockInput(true);self:performWithDelay(function()app:lockInput()end,0.5)
 
-        app:lockInput(true)
-        self:performWithDelay(function()
-            app:lockInput(false)
-        end, 0.5)
-
-        Sprite:PromiseOfFlash(unpack(buildings)):next(function()
+        Sprite:PromiseOfFlash(unpack(self:CollectBuildings(building))):next(function()
             if iskindof(building, "HelpedTroopsSprite") then
                 local helped = self.city:GetHelpedByTroops()[building:GetIndex()]
                 local user = self.city:GetUser()
--- dev/scripts/app/scenes/OtherCityScene.lua
+++ dev/scripts/app/scenes/OtherCityScene.lua
@@ -15,26 +15,8 @@ end
 function OtherCityScene:OnTouchClicked(pre_x, pre_y, x, y)
 	local building = self:GetSceneLayer():GetClickedObject(x, y)
     if building then
-        local buildings = {}
-        if building:GetEntity():GetType() == "wall" then
-            for i,v in ipairs(self:GetSceneLayer():GetWalls()) do
-                table.insert(buildings, v)
-            end
-            for i,v in ipairs(self:GetSceneLayer():GetTowers()) do
-                table.insert(buildings, v)
-            end
-        elseif building:GetEntity():GetType() == "tower" then
-            buildings = {unpack(self:GetSceneLayer():GetTowers())}
-        else
-            buildings = {building}
-        end
-
-        app:lockInput(true)
-        self:performWithDelay(function()
-            app:lockInput(false)
-        end, 0.5)
-
-        Sprite:PromiseOfFlash(unpack(buildings))
+        app:lockInput(true);self:performWithDelay(function()app:lockInput()end,0.5)
+        Sprite:PromiseOfFlash(unpack(self:CollectBuildings(building)))
     end
 end
 
