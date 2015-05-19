d832d3334bd7fd201aa6af05436c812028bb5c79
DannyHe <dannyjiajia@gmail.com>
2015-05-19 09:04:38 +0800

    uservoice sdk参数添加 talkingdata 参数配置

--- frameworks/sdk/uservoice/ext_uservoice.h
+++ frameworks/sdk/uservoice/ext_uservoice.h
@@ -8,5 +8,5 @@
 
 #ifndef __kod__ext_uservoice__
 #define __kod__ext_uservoice__
-extern void OpenUserVoice();
+extern void OpenUserVoice(const char* site,int forumId,const char* email,const char* user_name,const char* guid);
 #endif /* defined(__kod__ext_uservoice__) */
