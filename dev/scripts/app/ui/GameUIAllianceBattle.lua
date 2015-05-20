bca8a3bc732cf7ca0a1c6f12ca502803d5a2740a
Kenny Dai <daidai917@gmail.com>
2015-05-20 14:29:55 +0800

    bugfix

--- dev/scripts/app/ui/GameUIAllianceBattle.lua
+++ dev/scripts/app/ui/GameUIAllianceBattle.lua
@@ -857,6 +857,7 @@ function GameUIAllianceBattle:CreateHistoryContent()
         local fightTime = report.fightTime
         local ourAlliance = report.attackAllianceId == alliance:Id() and report.attackAlliance or report.defenceAlliance
         local enemyAlliance = report.attackAllianceId == alliance:Id() and report.defenceAlliance or report.attackAlliance
+        local killMax = report.killMax
 
         fight_time_label:setString(GameUtils:formatTimeStyle2(math.floor(fightTime/1000)))
         local win_text = win and _("胜利") or _("失败")
@@ -903,7 +904,7 @@ function GameUIAllianceBattle:CreateHistoryContent()
             {string.formatnumberthousands(ourAlliance.strikeSuccessCount),_("突袭成功"),string.formatnumberthousands(enemyAlliance.strikeSuccessCount)},
             {string.formatnumberthousands(ourAlliance.attackCount),_("进攻次数"),string.formatnumberthousands(enemyAlliance.attackCount)},
             {string.formatnumberthousands(ourAlliance.attackSuccessCount),_("进攻成功"),string.formatnumberthousands(enemyAlliance.attackSuccessCount)},
-            {"阿拉蕾",_("头号杀手"),"无"},
+            {killMax.allianceId == alliance:Id() and killMax.name or _("无"),_("头号杀手"),killMax.allianceId ~= alliance:Id() and killMax.name or _("无")},
             {string.formatnumberthousands(ourAlliance.honour),_("荣耀值奖励"),string.formatnumberthousands(enemyAlliance.honour)},
         }
         local b_flag = true
