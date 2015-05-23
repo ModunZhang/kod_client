ccab57e59568cdef35ee5eb1db575d5faef219e7
DannyHe <dannyjiajia@gmail.com>
2015-05-23 16:01:52 +0800

    新的lua配置文件

---
+++ dev/scripts/app/datas/AllianceBuilding_moonGate.lua
@@ -0,0 +1,62 @@
+local moonGate = GameDatas.AllianceBuilding.moonGate
+
+moonGate[1] = {
+	["level"] = 1,
+	["needHonour"] = 21500,
+	["defendingCount"] = 5,
+	["power"] = 215
+}
+moonGate[2] = {
+	["level"] = 2,
+	["needHonour"] = 43100,
+	["defendingCount"] = 6,
+	["power"] = 431
+}
+moonGate[3] = {
+	["level"] = 3,
+	["needHonour"] = 86300,
+	["defendingCount"] = 7,
+	["power"] = 863
+}
+moonGate[4] = {
+	["level"] = 4,
+	["needHonour"] = 172600,
+	["defendingCount"] = 8,
+	["power"] = 1726
+}
+moonGate[5] = {
+	["level"] = 5,
+	["needHonour"] = 345200,
+	["defendingCount"] = 9,
+	["power"] = 3452
+}
+moonGate[6] = {
+	["level"] = 6,
+	["needHonour"] = 690500,
+	["defendingCount"] = 10,
+	["power"] = 6905
+}
+moonGate[7] = {
+	["level"] = 7,
+	["needHonour"] = 1596700,
+	["defendingCount"] = 11,
+	["power"] = 15967
+}
+moonGate[8] = {
+	["level"] = 8,
+	["needHonour"] = 3005100,
+	["defendingCount"] = 12,
+	["power"] = 30051
+}
+moonGate[9] = {
+	["level"] = 9,
+	["needHonour"] = 5947100,
+	["defendingCount"] = 13,
+	["power"] = 59471
+}
+moonGate[10] = {
+	["level"] = 10,
+	["needHonour"] = 10981500,
+	["defendingCount"] = 14,
+	["power"] = 109815
+}
