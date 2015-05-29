a8666e4f02b12ad381243f098c4e25f876d33098
DannyHe <dannyjiajia@gmail.com>
2015-05-29 15:01:53 +0800

    整理图

--- tools/scripts/buildRes.sh
+++ tools/scripts/buildRes.sh
@@ -48,6 +48,28 @@ exportImagesRes()
 			fi
 		fi
 	done
+	echo -- 处理rgba444_single文件夹
+	for file in $images_dir/rgba444_single/*
+	do
+		if test -f "$file";then
+			finalDir=$outdir/${images_dir##*/res/}
+			outfile=$finalDir/${file##*/}
+			fileExt=${file##*.}
+			if test "$file" -nt "$outfile"; then
+				echo "---- ${file##*/}"
+				if test $fileExt == "plist" || test $fileExt == "ExportJson";then
+					test -d $finalDir || mkdir -p $finalDir && cp "$file" $finalDir
+				else
+					TexturePacker --format cocos2d --no-trim --disable-rotation --texture-format png --opt RGBA4444 --png-opt-level 7  --allow-free-size --padding 0 "$file" --sheet "$outfile"
+					# if $NEED_ENCRYPT_RES;then
+					# 	test -d $finalDir || mkdir -p $finalDir && $RES_COMPILE_TOOL -i "$file" -o $finalDir -ek $XXTEAKey -es $XXTEASign -q
+					# else
+					# 	test -d $finalDir || mkdir -p $finalDir && cp "$file" $finalDir
+					# fi
+				fi
+			fi
+		fi
+	done
 	echo -- 处理_CanCompress文件夹
 	for file in $images_dir/_CanCompress/*
 	do
