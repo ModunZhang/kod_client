8261473ce42dc582ad3c54ada90cd80e2f008556
DannyHe <dannyjiajia@gmail.com>
2015-05-31 17:50:02 +0800

    脚本修改

--- tools/scripts/buildScripts.sh
+++ tools/scripts/buildScripts.sh
@@ -31,12 +31,20 @@ exportScripts()
 exportScriptsEncrypt()
 {
 	outdir=$SCRIPTS_DEST_DIR
-	$SCRIPT_COMPILE_TOOL -i $SCRIPTS_SRC_DIR -o $outdir -m files -e xxtea_chunk -ex lua -ek $XXTEAKey -es $XXTEASign
+	outfile="$outdir/game.zip"
+	tempfile="$SCRIPTS_SRC_DIR/game.zip"
+	if $NEED_ENCRYPT_SCRIPTS; then
+		$SCRIPT_COMPILE_TOOL -i $SCRIPTS_SRC_DIR -o "$tempfile" -e xxtea_zip -ex lua -ek $XXTEAKey -es $XXTEASign -q
+	else
+		$SCRIPT_COMPILE_TOOL -i $SCRIPTS_SRC_DIR -o "$tempfile" -ex lua -q
+	fi
+	if test "$tempfile" -nt "$outfile"; then
+		echo 拷贝game.zip
+		mv -f "$tempfile" "$outfile"
+	else
+		echo 忽略game.zip
+		rm -f "$tempfile"
+	fi
 }
-
-if $NEED_ENCRYPT_SCRIPTS; then
-	exportScriptsEncrypt 
-else
-	exportScripts $SCRIPTS_SRC_DIR
-fi
-find $SCRIPTS_DEST_DIR -name "*.bytes" -exec rm -r {} \;
+exportScriptsEncrypt 
+find $SCRIPTS_SRC_DIR -name "*.bytes" -exec rm -rv {} \;
