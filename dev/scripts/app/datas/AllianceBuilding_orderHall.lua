3c7b36d11d238e5527f5c47856fdabd52cbac795
DannyHe <dannyjiajia@gmail.com>
2015-06-01 20:42:42 +0800

    新的配置lua文件

---
+++ dev/scripts/app/datas/AllianceBuilding_orderHall.lua
@@ -0,0 +1,62 @@
+local orderHall = GameDatas.AllianceBuilding.orderHall
+
+orderHall[1] = {
+	["level"] = 1,
+	["needHonour"] = 15500,
+	["villageCount"] = 4,
+	["power"] = 155
+}
+orderHall[2] = {
+	["level"] = 2,
+	["needHonour"] = 31000,
+	["villageCount"] = 5,
+	["power"] = 310
+}
+orderHall[3] = {
+	["level"] = 3,
+	["needHonour"] = 62100,
+	["villageCount"] = 6,
+	["power"] = 621
+}
+orderHall[4] = {
+	["level"] = 4,
+	["needHonour"] = 124200,
+	["villageCount"] = 7,
+	["power"] = 1242
+}
+orderHall[5] = {
+	["level"] = 5,
+	["needHonour"] = 248500,
+	["villageCount"] = 8,
+	["power"] = 2485
+}
+orderHall[6] = {
+	["level"] = 6,
+	["needHonour"] = 497100,
+	["villageCount"] = 9,
+	["power"] = 4971
+}
+orderHall[7] = {
+	["level"] = 7,
+	["needHonour"] = 1149600,
+	["villageCount"] = 10,
+	["power"] = 11496
+}
+orderHall[8] = {
+	["level"] = 8,
+	["needHonour"] = 2163700,
+	["villageCount"] = 11,
+	["power"] = 21637
+}
+orderHall[9] = {
+	["level"] = 9,
+	["needHonour"] = 4281900,
+	["villageCount"] = 13,
+	["power"] = 42819
+}
+orderHall[10] = {
+	["level"] = 10,
+	["needHonour"] = 7906600,
+	["villageCount"] = 15,
+	["power"] = 79066
+}
