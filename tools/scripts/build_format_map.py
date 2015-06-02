531e899d4a02df7c597c8b0714dd38b1398e46a6
gaozhou <gaozhou.f@qq.com>
2015-06-02 20:55:45 +0800

    修改bug

---
+++ tools/scripts/build_format_map.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os,sys,getopt,codecs
+
+
+
+if __name__=="__main__":
+	resource_dir = '../../dev/res/images/rgba444_single'
+	dest_dir = '../../dev/scripts/app/'
+	global lua_file
+
+	try:
+		opts,args = getopt.getopt(sys.argv[1:], 'd:')
+	except getopt.GetoptError:
+		sys.exit()
+
+	for opt, arg in opts:
+		if opt == '-d':
+			if not arg:
+				sys.exit(-1)
+			lua_file = arg
+
+	try:
+		with codecs.open(dest_dir + lua_file, 'w', 'utf-8') as f:
+			_, file_name = os.path.split(resource_dir)
+			f.write('local %s = {}\n' % file_name)
+			for root, dirs, files in os.walk(resource_dir):
+				for fileName in files:
+					f.write('%s["%s"] = cc.TEXTURE2_D_PIXEL_FORMAT_RGB_A4444\n' % (file_name, fileName) )
+			f.write('return %s' % file_name)
+	except:
+		sys.exit()
+
+
+
+
+
+
+
