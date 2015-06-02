24440bf5974e62fdcab81e149de8046025bd984f
gaozhou <gaozhou.f@qq.com>
2015-06-02 15:24:29 +0800

    修改bug

--- dev/scripts/app/ui/GameUIHasBeenBuild.lua
+++ dev/scripts/app/ui/GameUIHasBeenBuild.lua
@@ -270,7 +270,7 @@ function Item:UpdateByBuilding(building, current_time)
     self:SetBuildingType(building:GetType(), building:GetLevel())
     repeat
         if building:IsUpgrading() then
-            assert(current_time ~= 0)
+            -- assert(current_time ~= 0)
             local can_free_speedUp = building:GetUpgradingLeftTimeByCurrentTime(current_time) <= DataUtils:getFreeSpeedUpLimitTime()
             self:ChangeStatus(can_free_speedUp and "free" or "building")
             self:UpdateProgress(building)
@@ -308,14 +308,18 @@ function Item:UpdateDesc(building)
     if building:IsUpgrading() then
         if building:GetNextLevel() == 1 then
             self.desc_label:setString(building:IsHouse() and _("正在建造") or _("正在解锁"))
+            self.desc_label:setPositionY(35)
         else
             self.desc_label:setString(string.format("%s%d", _("正在升级到 等级"), building:GetNextLevel()))
+            self.desc_label:setPositionY(35)
         end
     else
         if building:IsMaxLevel() then
             self.desc_label:setString(string.format("%s", _("已经到最大等级了")))
+            self.desc_label:setPositionY(70)
         else
             self.desc_label:setString(string.format("%s%d%s%d", _("从等级"), building:GetLevel(), _("升级到等级"), building:GetNextLevel()))
+            self.desc_label:setPositionY(35)
         end
     end
 end
