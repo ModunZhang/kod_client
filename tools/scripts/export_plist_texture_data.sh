73824f5df62c8288f28b97a0ea63a1a0ba05fe40
DannyHe <dannyjiajia@gmail.com>
2015-06-09 15:02:53 +0800

    完善脚本

--- tools/scripts/export_plist_texture_data.sh
+++ tools/scripts/export_plist_texture_data.sh
@@ -1,2 +1,38 @@
 #! /bin/bash
 #通过plist文件生成包含大图信息的lua文件
+####################################
+PLATFORMS="iOS Player"
+getPlatform()
+{
+Platform=$1
+if [[ -z $Platform ]]
+then
+    echo "Platform :" >&2
+    select Platform in $PLATFORMS
+    do
+        if [[ -n $Platform ]]
+        then
+            break
+        fi
+    done
+fi
+echo $Platform
+}
+####################################
+
+Platform=`getPlatform $1`
+SCRIPTS_SRC_DIR=`./functions.sh getScriptsDir`
+RES_SRC_DIR=`./functions.sh getResourceDir`
+EXPORT_FILE_PATH=""
+PLIST_PATH=""
+
+if [[ $Platform == 'iOS' ]]; then
+	EXPORT_FILE_PATH="texture_data_iOS.lua"
+	PLIST_PATH="${RES_SRC_DIR}/images/_Compressed"
+elif [[ $Platform == 'Player' ]]; then
+	EXPORT_FILE_PATH="texture_data.lua"
+	PLIST_PATH="${RES_SRC_DIR}/images/_Compressed_mac"
+fi
+EXPORT_FILE_PATH="${SCRIPTS_SRC_DIR}/app/${EXPORT_FILE_PATH}"
+# run
+python plist_texture_data_to_lua.py -p "$PLIST_PATH" -o "$EXPORT_FILE_PATH"
