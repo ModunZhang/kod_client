131dde5bb036148780217670ebc8851c1f158fd1
Kenny Dai <daidai917@gmail.com>
2015-07-09 15:23:13 +0800

    有关于自己的行军事件时不能移城

--- dev/scripts/app/widget/WidgetUseItems.lua
+++ dev/scripts/app/widget/WidgetUseItems.lua
@@ -795,28 +795,35 @@ function WidgetUseItems:OpenMoveTheCityDialog( item ,params)
             return true
         end,
         function ()
-            local item_name = item:Name()
-            NetManager:getUseItemPromise(item_name,{
-                [item_name]={
-                    locationX = params.locationX,
-                    locationY = params.locationY
-                }
+            if Alliance_Manager:GetMyAlliance():GetAllianceBelvedere():HaveEvent() then
+                UIKit:showMessageDialog(_("提示"),_("当前不能移城"))
+            else
+                local item_name = item:Name()
+                NetManager:getUseItemPromise(item_name,{
+                    [item_name]={
+                        locationX = params.locationX,
+                        locationY = params.locationY
+                    }
 
-            }):done(function ()
-                dialog:LeftButtonClicked()
-            end)
+                }):done(function ()
+                    dialog:LeftButtonClicked()
+                end)
+            end
         end,
         function ()
-            local item_name = item:Name()
-            NetManager:getBuyAndUseItemPromise(item_name,{
-                [item_name]={
-                    locationX = params.locationX,
-                    locationY = params.locationY
-                }
-
-            }):done(function ()
-                dialog:LeftButtonClicked()
-            end)
+            if Alliance_Manager:GetMyAlliance():GetAllianceBelvedere():HaveEvent() then
+                UIKit:showMessageDialog(_("提示"),_("当前不能移城"))
+            else
+                local item_name = item:Name()
+                NetManager:getBuyAndUseItemPromise(item_name,{
+                    [item_name]={
+                        locationX = params.locationX,
+                        locationY = params.locationY
+                    }
+                }):done(function ()
+                    dialog:LeftButtonClicked()
+                end)
+            end
         end
     ):addTo(item_box_bg):align(display.CENTER,item_box_bg:getContentSize().width/2,item_box_bg:getContentSize().height/2)
     return dialog
@@ -1335,6 +1342,7 @@ end
 
 
 return WidgetUseItems
+
 
 
 
