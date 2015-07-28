d28161e70dfed0c9b7827319a8cc1144c8472abd
gaozhou <gaozhou.f@qq.com>
2015-07-28 15:47:23 +0800

    添加ui

---
+++ dev/scripts/app/ui/GameUIPveAttack.lua
@@ -0,0 +1,9 @@
+local UILib = import(".UILib")
+local Localize = import("..utils.Localize")
+local window = import("..utils.window")
+local WidgetPopDialog = import("..widget.WidgetPopDialog")
+local GameUIPveAttack = class("GameUIPveAttack", WidgetPopDialog)
+
+function GameUIPveAttack:ctor()
+
+end
