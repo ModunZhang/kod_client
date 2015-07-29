c103595cdc79d8e6bcf2caf4296a61f1a72a3d12
gaozhou <gaozhou.f@qq.com>
2015-07-29 17:09:21 +0800

    关掉pve

--- dev/scripts/app/scenes/MyCityScene.lua
+++ dev/scripts/app/scenes/MyCityScene.lua
@@ -415,12 +415,12 @@ function MyCityScene:OpenUI(building, default_tab, need_tips, build_name)
         if #dragon_type > 0 or dragon_manger:GetDefenceDragon() then
             -- local _,_,index = self.city:GetUser():GetPVEDatabase():GetCharPosition()
             -- app:EnterPVEScene(index)
-            NetManager:getAttackPveSectionPromise("1_1", "greenDragon", {
-                {
-                    name = "swordsman",
-                    count = 100
-                }
-            })
+            -- NetManager:getAttackPveSectionPromise("1_1", "greenDragon", {
+            --     {
+            --         name = "swordsman",
+            --         count = 100
+            --     }
+            -- })
         else
             UIKit:showMessageDialog(_("主人"),_("需要一条空闲状态的魔龙才能探险"))
         end
