4ac7946d1de5ad8f64cd61acf6ce2b4bfb6f1223
Kenny Dai <daidai917@gmail.com>
2015-08-13 15:34:11 +0800

    更新配置

--- dev/scripts/app/datas/ProductionTechs_productionTechs.lua
+++ dev/scripts/app/datas/ProductionTechs_productionTechs.lua
@@ -85,7 +85,7 @@ productionTechs["trap"] = {
 	["index"] = 11,
 	["unlockBy"] = 8,
 	["unlockLevel"] = 2,
-	["academyLevel"] = 24,
+	["academyLevel"] = 20,
 	["effectPerLevel"] = 0.010000
 }
 productionTechs["healingAgent"] = {
@@ -93,7 +93,7 @@ productionTechs["healingAgent"] = {
 	["index"] = 12,
 	["unlockBy"] = 9,
 	["unlockLevel"] = 2,
-	["academyLevel"] = 30,
+	["academyLevel"] = 18,
 	["effectPerLevel"] = 0.010000
 }
 productionTechs["colonization"] = {
@@ -101,7 +101,7 @@ productionTechs["colonization"] = {
 	["index"] = 13,
 	["unlockBy"] = 10,
 	["unlockLevel"] = 2,
-	["academyLevel"] = 20,
+	["academyLevel"] = 24,
 	["effectPerLevel"] = 0.010000
 }
 productionTechs["hideout"] = {
@@ -117,7 +117,7 @@ productionTechs["sketching"] = {
 	["index"] = 15,
 	["unlockBy"] = 12,
 	["unlockLevel"] = 2,
-	["academyLevel"] = 28,
+	["academyLevel"] = 22,
 	["effectPerLevel"] = 0.010000
 }
 productionTechs["recruitment"] = {
@@ -125,7 +125,7 @@ productionTechs["recruitment"] = {
 	["index"] = 16,
 	["unlockBy"] = 13,
 	["unlockLevel"] = 2,
-	["academyLevel"] = 18,
+	["academyLevel"] = 30,
 	["effectPerLevel"] = 0.010000
 }
 productionTechs["logistics"] = {
@@ -133,7 +133,7 @@ productionTechs["logistics"] = {
 	["index"] = 17,
 	["unlockBy"] = 14,
 	["unlockLevel"] = 2,
-	["academyLevel"] = 22,
+	["academyLevel"] = 28,
 	["effectPerLevel"] = 0.020000
 }
 productionTechs["mintedCoin"] = {
