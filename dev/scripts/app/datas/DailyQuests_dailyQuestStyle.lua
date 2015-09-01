bccc99012c18a2511d48dc500eb2d15ad0b4b15e
gaozhou <gaozhou.f@qq.com>
2015-09-01 22:44:58 +0800

    修改配置表

--- dev/scripts/app/datas/DailyQuests_dailyQuestStyle.lua
+++ dev/scripts/app/datas/DailyQuests_dailyQuestStyle.lua
@@ -2,7 +2,7 @@ local dailyQuestStyle = GameDatas.DailyQuests.dailyQuestStyle
 
 dailyQuestStyle[1] = {
 	["style"] = 1,
-	["star_1"] = 4,
+	["star_1"] = 2,
 	["star_2"] = 4,
 	["star_3"] = 2,
 	["star_4"] = 0,
@@ -10,7 +10,7 @@ dailyQuestStyle[1] = {
 }
 dailyQuestStyle[2] = {
 	["style"] = 2,
-	["star_1"] = 5,
+	["star_1"] = 3,
 	["star_2"] = 3,
 	["star_3"] = 1,
 	["star_4"] = 1,
@@ -18,7 +18,7 @@ dailyQuestStyle[2] = {
 }
 dailyQuestStyle[3] = {
 	["style"] = 3,
-	["star_1"] = 6,
+	["star_1"] = 4,
 	["star_2"] = 2,
 	["star_3"] = 1,
 	["star_4"] = 1,
@@ -26,7 +26,7 @@ dailyQuestStyle[3] = {
 }
 dailyQuestStyle[4] = {
 	["style"] = 4,
-	["star_1"] = 7,
+	["star_1"] = 5,
 	["star_2"] = 1,
 	["star_3"] = 1,
 	["star_4"] = 1,
@@ -34,7 +34,7 @@ dailyQuestStyle[4] = {
 }
 dailyQuestStyle[5] = {
 	["style"] = 5,
-	["star_1"] = 8,
+	["star_1"] = 6,
 	["star_2"] = 0,
 	["star_3"] = 1,
 	["star_4"] = 0,
